a43bfe0f0dd21e3ad1daa30473532e0262d631b8
Emily Bache <emily.bache@gmail.com>
2012-01-12 21:55:28 +0100

    Java version of the kata now compiles and has a failing test

---
+++ GildedRose/Java/GildedRoseTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+	@Test
+	public void foo() {
+		Item[] items = new Item[] {
+				new Item("foo", 0, 0)
+		};
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals("fixme", app.items[0].name);
+	}
+}
