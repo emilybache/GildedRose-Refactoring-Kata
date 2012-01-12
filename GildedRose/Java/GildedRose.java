5b6c47ccc8b05eda7d74251e727e57b5d86ae151
Emily Bache <emily.bache@gmail.com>
2012-01-12 22:05:13 +0100

    formatted code according to eclipse's formatter

--- GildedRose/Java/GildedRose.java
+++ GildedRose/Java/GildedRose.java
@@ -1,103 +1,77 @@
-
 class GildedRose {
-    Item[] items;
+	Item[] items;
 
-    public GildedRose(Item[] items) {
-    	this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
 	public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+		System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-              new Item("+5 Dexterity Vest", 10, 20),
-              new Item("Aged Brie", 2, 0),
-              new Item("Elixir of the Mongoose", 5, 7),
-              new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-              new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-              new Item("Conjured Mana Cake", 3, 6)
-          };
+		Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20),
+				new Item("Aged Brie", 2, 0),
+				new Item("Elixir of the Mongoose", 5, 7),
+				new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+				new Item("Conjured Mana Cake", 3, 6) };
 
-        GildedRose app = new GildedRose(items);
+		GildedRose app = new GildedRose(items);
 
-        app.updateQuality();
+		app.updateQuality();
 
-    }
+	}
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++)
-        {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (items[i].quality > 0)
-                {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality = items[i].quality + 1;
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
+			if (items[i].name != "Aged Brie"
+					&& items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+				if (items[i].quality > 0) {
+					if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+						items[i].quality = items[i].quality - 1;
+					}
+				}
+			} else {
+				if (items[i].quality < 50) {
+					items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (items[i].sellIn < 11)
-                        {
-                            if (items[i].quality < 50)
-                            {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+					if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+						if (items[i].sellIn < 11) {
+							if (items[i].quality < 50) {
+								items[i].quality = items[i].quality + 1;
+							}
+						}
 
-                        if (items[i].sellIn < 6)
-                        {
-                            if (items[i].quality < 50)
-                            {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+						if (items[i].sellIn < 6) {
+							if (items[i].quality < 50) {
+								items[i].quality = items[i].quality + 1;
+							}
+						}
+					}
+				}
+			}
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros")
-            {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+			if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+				items[i].sellIn = items[i].sellIn - 1;
+			}
 
-            if (items[i].sellIn < 0)
-            {
-                if (items[i].name != "Aged Brie")
-                {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (items[i].quality > 0)
-                        {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                }
-                else
-                {
-                    if (items[i].quality < 50)
-                    {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
+			if (items[i].sellIn < 0) {
+				if (items[i].name != "Aged Brie") {
+					if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+						if (items[i].quality > 0) {
+							if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+								items[i].quality = items[i].quality - 1;
+							}
+						}
+					} else {
+						items[i].quality = items[i].quality - items[i].quality;
+					}
+				} else {
+					if (items[i].quality < 50) {
+						items[i].quality = items[i].quality + 1;
+					}
+				}
+			}
+		}
+	}
 
 }
-
--- GildedRose/Java/GildedRoseTest.java
+++ GildedRose/Java/GildedRoseTest.java
@@ -6,9 +6,7 @@ public class GildedRoseTest {
 
 	@Test
 	public void foo() {
-		Item[] items = new Item[] {
-				new Item("foo", 0, 0)
-		};
+		Item[] items = new Item[] { new Item("foo", 0, 0) };
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
 		assertEquals("fixme", app.items[0].name);
