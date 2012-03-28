c73ac70498ac75aa4933f2747b2f63ba4fe048e6
Emily <emily@sofa-pc.(none)>
2012-03-28 15:02:21 +0200

    updating python, Java and cpp versions to all work with the same texttests

--- GildedRose/Java/Main.java
+++ GildedRose/Java/Main.java
@@ -7,7 +7,11 @@ public class Main {
 				new Item("Aged Brie", 2, 0),
 				new Item("Elixir of the Mongoose", 5, 7),
 				new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+				new Item("Sulfuras, Hand of Ragnaros", -1, 80),
 				new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+				// this conjured item does not work properly yet
 				new Item("Conjured Mana Cake", 3, 6) };
 
 		GildedRose app = new GildedRose(items);
--- GildedRose/cpp/GildedRoseTextTests.cc
+++ GildedRose/cpp/GildedRoseTextTests.cc
@@ -17,7 +17,11 @@ int main()
     items.push_back(Item("Aged Brie", 2, 0));
     items.push_back(Item("Elixir of the Mongoose", 5, 7));
     items.push_back(Item("Sulfuras, Hand of Ragnaros", 0, 80));
+    items.push_back(Item("Sulfuras, Hand of Ragnaros", -1, 80));
     items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
+    // this Conjured item doesn't yet work properly
     items.push_back(Item("Conjured Mana Cake", 3, 6));
     GildedRose app(items);
 
