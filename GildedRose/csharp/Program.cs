2e5a38d0008a50d595436cb7a7bf00d62ea8747c
Emily Bache <emily@bacheconsulting.com>
2012-03-28 20:59:53 +0200

    make c sharp version work together with texttests

--- GildedRose/csharp/Program.cs
+++ GildedRose/csharp/Program.cs
@@ -17,19 +17,42 @@ namespace GildedRose.Console
                                               new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                                               new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                                               new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
                                               new Item
                                                   {
                                                       Name = "Backstage passes to a TAFKAL80ETC concert",
                                                       SellIn = 15,
                                                       Quality = 20
                                                   },
+                                              new Item
+                                                  {
+                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
+                                                      SellIn = 10,
+                                                      Quality = 49
+                                                  },
+                                              new Item
+                                                  {
+                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
+                                                      SellIn = 5,
+                                                      Quality = 49
+                                                  },
+                                              // this conjured item does not work properly yet
                                               new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
                                           }
 
                           };
 
-            app.UpdateQuality();
-
+            for (var i = 0; i < 31; i++)
+            {
+                System.Console.WriteLine("-------- day " + i + " --------");
+                System.Console.WriteLine("name, sellIn, quality");
+                for (var j = 0; j < app.Items.Count; j++)
+                {
+                    System.Console.WriteLine(app.Items[j].Name + ", " + app.Items[j].SellIn + ", " + app.Items[j].Quality);
+                }
+                System.Console.WriteLine("");
+                app.UpdateQuality();
+            }
             System.Console.ReadKey();
 
         }
