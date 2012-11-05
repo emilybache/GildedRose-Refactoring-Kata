ca6f1a5b555e579b8ec4f181df10ec878a69d9f8
emilybache <emily@bacheconsulting.com>
2012-11-05 20:58:35 +0100

    change to using unittest instead of py.test since it comes with the standard library

--- GildedRose/python/test_gilded_rose.py
+++ GildedRose/python/test_gilded_rose.py
@@ -1,6 +1,13 @@
+import unittest
+
 from gilded_rose import Item, update_quality
 
-def test_foo():
-    items = [Item("foo", 0, 0)]
-    update_quality(items)
-    assert "fixme" == items[0].name
+class GildedRoseTest(unittest.TestCase):
+    def test_foo(self):
+        items = [Item("foo", 0, 0)]
+        update_quality(items)
+        self.assertEquals("fixme", items[0].name)
+
+
+if __name__ == "__main__":
+    unittest.main()
