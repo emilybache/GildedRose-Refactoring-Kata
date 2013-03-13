4dab932c94f144fb4c49e68e53302a0c5065c0f4
Emily Bache <emily@bacheconsulting.com>
2013-03-13 18:20:38 +0100

    updated these versions to more closely match the original C#

--- GildedRose/python/test_gilded_rose.py
+++ GildedRose/python/test_gilded_rose.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 import unittest
-from gilded_rose import Item, update_quality
+
+from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
         items = [Item("foo", 0, 0)]
-        update_quality(items)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
