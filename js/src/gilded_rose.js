5af2bcd4ac25b35895c3ef3371840f986209112b
Peter Kofler <peter.kofler@code-cop.org>
2014-05-02 14:52:25 +0200

    add very basic texttest firxture for JavaScript

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -6,13 +6,6 @@ function Item(name, sell_in, quality) {
 
 var items = []
 
-items.push(new Item('+5 Dexterity Vest', 10, 20));
-items.push(new Item('Aged Brie', 2, 0));
-items.push(new Item('Elixir of the Mongoose', 5, 7));
-items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-items.push(new Item('Conjured Mana Cake', 3, 6));
-
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
