caf07e34fa574f2a8f798570bad0f77ac85c8901
Philip Schwarz <pschwarz@expedia.com>
2016-07-02 19:34:15 +0100

    undo small change to fixture and to Golden Master

--- Java/com/gildedrose/TexttestFixture.java
+++ Java/com/gildedrose/TexttestFixture.java
@@ -24,7 +24,7 @@ public class TexttestFixture {
                 new Item("Elixir of the Mongoose", 5, 7), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 200),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
