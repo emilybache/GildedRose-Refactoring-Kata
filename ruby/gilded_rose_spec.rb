778db32d9ebf863faa9bffcd3f0ad16a4a54f192
Dani Latorre <dani@danilat.com>
2016-08-02 15:03:07 +0200

    should syntax is deprecated, changed the first test to expect syntax

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -6,7 +6,7 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      items[0].name.should == "fixme"
+      expect(items[0].name).to eq "fixme"
     end
   end
 
