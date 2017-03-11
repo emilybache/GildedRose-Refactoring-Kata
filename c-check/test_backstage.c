264ed0c974e68c4167847bf372f86d401d90ee72
Clay Dowling <clay@lazarusid.com>
2017-03-10 22:42:52 -0500

    Tests for backstage passes

--- c-check/test_backstage.c
+++ c-check/test_backstage.c
@@ -24,6 +24,35 @@ START_TEST(backstage_when10Days_increasesByTwo)
 }
 END_TEST
 
+START_TEST(backstage_when5Days_increasesByThree)
+{
+  Item items[1];
+  init_item(items, GOOD_BACKSTAGE, 5, 15);
+  update_quality(items, 1);
+
+  ck_assert_int_eq(18, items[0].quality);
+}
+END_TEST
+
+START_TEST(backstage_when0Days_hasQualityZero)
+{
+  Item items[1];
+  init_item(items, GOOD_BACKSTAGE, 0, 15);
+  update_quality(items, 1);
+
+  ck_assert_int_eq(0, items[0].quality);
+}
+END_TEST
+
+START_TEST(otherBackstage_when12Days_decreasesByOne)
+{
+  Item items[1];
+  init_item(items, BAD_BACKSTAGE, 12, 15);
+  update_quality(items, 1);
+
+  ck_assert_int_eq(14, items[0].quality);
+}
+END_TEST
 
 TCase *tcase_backstage(void)
 {
@@ -32,6 +61,9 @@ TCase *tcase_backstage(void)
   tc = tcase_create("backstage-pass");
   tcase_add_test(tc, backstage_whenMoreThan10Days_increasesByOne);
   tcase_add_test(tc, backstage_when10Days_increasesByTwo);
+  tcase_add_test(tc, backstage_when5Days_increasesByThree);
+  tcase_add_test(tc, backstage_when0Days_hasQualityZero);
+  tcase_add_test(tc, otherBackstage_when12Days_decreasesByOne);
 
   return tc;
 }
