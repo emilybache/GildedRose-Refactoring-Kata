e40b42bff743d6fe95ef82819df52c2339a49aec
Diego Sousa <diego@synctech.com.br>
2017-07-16 14:56:36 -0300

    Added files needed to run the project on VS/15/17

---
+++ csharp/GildedRose/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace GildedRose
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
