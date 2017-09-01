620ad8069a77cd8736087ceda7c03fdb1ff24081
Diego Sousa <diegors90@gmail.com>
2017-08-31 23:35:31 -0300

    Corrections in csharp project

--- csharp/GildedRose/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GildedRose
-{
-    public class Item
-    {
-        public string Name { get; set; }
-        public int SellIn { get; set; }
-        public int Quality { get; set; }
-    }
-}
