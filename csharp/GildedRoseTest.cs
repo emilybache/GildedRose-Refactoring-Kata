23166143a28514b5921229f1f0ee75b491b9f092
Diego Sousa <diegors90@gmail.com>
2017-09-01 00:00:42 -0300

    Added files needed to run the csharp project on VS15/17

---
+++ csharp/GildedRoseTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("fixme", Items[0].Name);
+        }
+    }
+}
