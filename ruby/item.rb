4a7c5cef3fe2a50203813618a091151eda0958d3
Cyril Lepagnot <cyril@lepagnot.fr>
2017-09-26 10:09:34 +0200

    Use OpenStruct for item

--- ruby/item.rb
+++ ruby/item.rb
@@ -1,13 +1,7 @@
-class Item
-  attr_accessor :name, :sell_in, :quality
+require 'ostruct'
 
-  def initialize(name:, sell_in:, quality:)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
+class Item < OpenStruct
+  def to_s
+    [name, sell_in, quality].join ', '
   end
 end
