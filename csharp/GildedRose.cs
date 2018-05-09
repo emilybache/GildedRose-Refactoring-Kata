958f757cc402c708848e64f8b167ca0816a80f75
Radek <karlik.radoslav@gmail.com>
2018-05-09 22:01:12 +0200

    Refactor method

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,94 +1,71 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 
 namespace csharp
 {
     public class GildedRose
     {
+
         private readonly IList<Item> _items;
 
         public GildedRose(IList<Item> items)
             => _items = items;
 
+        private static int GetQualityChange(Item item)
+        {
+            var sellIn = item.SellIn;
+
+            switch (item.Name)
+            {
+                case "Sulfuras, Hand of Ragnaros":
+                    return 0;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (sellIn > 9)
+                    {
+                        return 1;
+                    }
+                    else if (sellIn > 4)
+                    {
+                        return 2;
+                    }
+                    else if (sellIn >= 0)
+                    {
+                        return 3;
+                    }
+                    else
+                    {
+                        return -item.Quality;
+                    }
+                case "Aged Brie":
+                    return sellIn < 0 ? 2 : 1;
+                case "Conjured Mana Cake":
+                    return sellIn < 0 ? -4 : -2;
+                default:
+                    return sellIn < 0 ? -2 : -1;
+            }
+        }
+
         public void UpdateQuality()
         {
             foreach (var item in _items)
             {
-                var qualityDegradationMultiplier = item.Name == "Conjured Mana Cake" ? 2 : 1;
+                item.SellIn--;
 
-                if (item.Name != "Aged Brie"
-                    && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality -= qualityDegradationMultiplier;
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality++;
+                var qualityChange = GetQualityChange(item);
 
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality++;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality++;
-                                }
-                            }
-                        }
-                    }
-                }
+                item.Quality += qualityChange;
 
                 if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    item.SellIn--;
-                }
-
-                if (item.SellIn >= 0)
-                {
-                    continue;
-                }
-
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality <= 0)
-                        {
-                            continue;
-                        }
-
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality -= qualityDegradationMultiplier;
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = 0;
-                    }
+                    item.Quality = Math.Min(50, item.Quality);
                 }
                 else
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality++;
-                    }
+                    item.SellIn++;
                 }
+
+                item.Quality = Math.Max(0, item.Quality);
             }
         }
     }
