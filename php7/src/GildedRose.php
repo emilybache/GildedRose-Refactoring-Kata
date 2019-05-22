216f754aeea42a8c917bd6938f3784f86ff0f6f1
Kevin Boyd <kboyd@smugmug.com>
2019-05-22 16:46:01 -0700

    Relocate special Sulfuras exception to be more buried.

--- php7/src/GildedRose.php
+++ php7/src/GildedRose.php
@@ -12,16 +12,12 @@ final class GildedRose {
 
     public function updateQuality() {
         foreach ($this->items as $item) {
-            if ($item->name === 'Sulfuras, Hand of Ragnaros') {
-                $item->quality = 80;
-
-                continue;
-            }
-
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
                         $item->quality = $item->quality - 1;
+                    } else {
+                        $item->quality = 80;
                     }
                 }
             } else {
