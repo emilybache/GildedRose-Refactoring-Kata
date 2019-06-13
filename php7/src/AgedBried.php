e6e459351d6d0cc002b081314b81da823bef1658
saulius12 <sauliusaulys@gmail.com>
2019-06-13 19:08:26 +0300

    separate into classes

---
+++ php7/src/AgedBried.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App;
+
+
+class AgedBried
+{
+
+}
---
+++ php7/src/DefaultItem.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App;
+
+
+class DefaultItem extends GildedRose
+{
+    private $item;
+
+    public function __construct($item) {
+        $this->item = $item;
+    }
+    public function updateQuality(){
+        $this->item->quality -= 1;
+        if ($this->item->sell_in <= 0) {
+            $this->item->quality -= 1;
+        }
+        $this->item->sell_in -= 1;
+
+    }
+}
