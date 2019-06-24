caf76059af8ef3171befc3fcfca3223b4c0fbaf2
Konstantin Pavlov <git@konstantinpavlov.net>
2019-06-24 05:54:31 +0200

    upgrade junit

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,9 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class GildedRoseTest {
 
