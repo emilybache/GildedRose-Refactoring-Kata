534dd114e36e568f474b6e04b806bf052e4a8334
Konstantin Pavlov <git@konstantinpavlov.net>
2019-06-24 06:04:38 +0200

    fix foo test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,17 +2,25 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
-public class GildedRoseTest {
+class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void shouldProcessFooItem() {
+        // given
+        final Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
+
+        // when
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        // then
+        final Item item = app.items[0];
+        assertThat(item.name).isEqualTo("foo");
+        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.sellIn).isEqualTo(-1);
     }
 
 }
