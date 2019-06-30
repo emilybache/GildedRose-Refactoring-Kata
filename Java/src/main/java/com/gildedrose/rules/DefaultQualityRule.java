9da12c47533763f290953008e6bd2da8106cdae1
Konstantin Pavlov <git@konstantinpavlov.net>
2019-06-30 06:45:15 +0200

    degrade quality for overdue items

--- Java/src/main/java/com/gildedrose/rules/DefaultQualityRule.java
+++ Java/src/main/java/com/gildedrose/rules/DefaultQualityRule.java
@@ -11,7 +11,13 @@ public class DefaultQualityRule implements QualityRule {
 
     @Override
     public Result calculateQuality(int oldQuality, int sellIn) {
-        return new Result(max(oldQuality - 1, 0), false);
+        final int newQuality;
+        if (sellIn < 0) {
+            newQuality = oldQuality - 2;
+        } else {
+            newQuality = oldQuality - 1;
+        }
+        return new Result(max(newQuality, 0), false);
     }
 
 }
