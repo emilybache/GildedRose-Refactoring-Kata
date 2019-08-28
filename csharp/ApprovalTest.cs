fbcc8aaa4ddddc67bf0017a50b6f04311d795eb4
Emily Bache <emily@bacheconsulting.com>
2019-08-28 10:30:34 +0200

    fix approval tests to work on windows, and use Approvals framework

--- csharp/ApprovalTest.cs
+++ csharp/ApprovalTest.cs
@@ -1,30 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
 using NUnit.Framework;
 
 namespace csharp
 {
+    [UseReporter(typeof(DiffReporter))]
     [TestFixture]
     public class ApprovalTest
     {
         [Test]
         public void ThirtyDays()
         {
-            var lines = File.ReadAllLines("ThirtyDays.txt");
-
+            
             StringBuilder fakeoutput = new StringBuilder();
             Console.SetOut(new StringWriter(fakeoutput));
             Console.SetIn(new StringReader("a\n"));
 
             Program.Main(new string[] { });
-            String output = fakeoutput.ToString();
+            var output = fakeoutput.ToString();
 
-            var outputLines = output.Split('\n');
-            for(var i = 0; i<Math.Min(lines.Length, outputLines.Length); i++) 
-            {
-                Assert.AreEqual(lines[i], outputLines[i]);
-            }
+            Approvals.Verify(output);
         }
     }
 }
