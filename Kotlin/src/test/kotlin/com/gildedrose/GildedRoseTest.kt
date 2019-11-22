80747e7c6d4e467897019fa7ade3148beefd87ae
OlegRyz <aleh.ryzhankou@gmail.com>
2019-11-22 22:49:20 +0100

    Fix fixme

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,15 +1,10 @@
 package com.gildedrose
 
-import org.junit.Assert.*
 import org.junit.Test
 
 class GildedRoseTest {
 
     @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
 
     }
 
