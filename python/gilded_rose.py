80b06af3886ac6197475451c4d561f8ed2883f53
alexmerryman <alexandermerryman2018@u.northwestern.edu>
2020-02-26 23:09:15 -0600

    Created new branch 'alex-refactor' for my edits

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,33 +7,43 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+
+            # item.name is something other than "Aged Brie" or "Backstage passes to a TAFKAL80ETC concert"
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
+
+                    # item.name is something other than "Sulfuras, Hand of Ragnaros"
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality -= 1
+
+            # item.name == "Aged Brie" OR "Backstage passes to a TAFKAL80ETC concert"
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality += 1
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality += 1
+
+
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
+
+
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                item.quality -= 1
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality -= item.quality
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality += 1
 
 
 class Item:
