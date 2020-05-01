417fe40c7fef99dae5dfdfb2ca39d9e158edd9ec
Povilas Brilius <pbrilius@gmail.com>
2020-05-01 19:05:17 +0300

    Unit test documentation - general.

--- php7/test/GildedRoseTest.php
+++ php7/test/GildedRoseTest.php
@@ -1,9 +1,29 @@
 <?php
 
+/**
+ * PHP version 7
+ * 
+ * @category PHP_Unit
+ * @package  Drill_Field
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
 namespace App;
 
-class GildedRoseTest extends \PHPUnit\Framework\TestCase {
-    public function testFoo() {
+/**
+ * Units cover up
+ * 
+ * @category PHP_Unit
+ * @package  GildenRose
+ * @author   Povilas Brilius <pbrilius@gmail.com>
+ * @license  eupl-1.1 https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+ * @link     pbgroupeu.wordpress.com
+ */
+class GildedRoseTest extends \PHPUnit\Framework\TestCase
+{
+    public function testFoo()
+    {
         $items      = [new Item("foo", 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
