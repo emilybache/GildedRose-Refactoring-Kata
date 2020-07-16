aa20f223ed5be70ff63ecdceef6069ea02e47b66
I24004 <stephane.a.genicot@bnpparibasfortis.com>
2020-07-16 16:16:20 +0200

    removed unused languages

--- csharpcore/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace csharpcore
-{
-    public class Item
-    {
-        public string Name { get; set; }
-        public int SellIn { get; set; }
-        public int Quality { get; set; }
-    }
-}
