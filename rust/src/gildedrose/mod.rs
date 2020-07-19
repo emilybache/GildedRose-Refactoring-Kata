87f3e536a405529d4fd493a7a2ec28c132f08c6b
rrokkam <rohithrokkam@gmail.com>
2020-07-19 12:38:24 -0700

    Risky: Move tests into mod.rs
    Usual Rust practice is to put tests in the same file they are testing.
    I don't see any magic in the texttests that would require tests and code
    to be in separate files, but I am not entirely sure, so I added the
    risky tag.

--- rust/src/gildedrose/mod.rs
+++ rust/src/gildedrose/mod.rs
@@ -78,4 +78,15 @@ impl GildedRose {
 }
 
 #[cfg(test)]
-mod test;
+mod tests {
+    use super::{GildedRose, Item};
+
+    #[test]
+    pub fn foo() {
+        let items = vec![Item::new(String::from("foo"), 0, 0)];
+        let mut rose = GildedRose::new(items);
+        rose.update_quality();
+
+        assert_eq!("fixme", rose.items[0].name);
+    }
+}
