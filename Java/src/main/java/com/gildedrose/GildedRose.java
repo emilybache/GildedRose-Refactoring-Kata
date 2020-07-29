4ca30eb7b2735e8898997909329e83e45e866790
piperfe <felipe.alfaro@walmart.com>
2020-07-29 18:22:47 -0400

    Unkwon Check

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -25,7 +25,6 @@ class GildedRose {
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        //items[i].quality = items[i].quality - 1;
                     }
                 }
             } else {
@@ -57,7 +56,6 @@ class GildedRose {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                         //       items[i].quality = items[i].quality - 1;
                             }
                         }
                     } else {
