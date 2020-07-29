3aa58d892d892a1e20de697d364f8263cdd59ce2
iliana <iliana.marcano@walmart.com>
2020-07-29 17:36:06 -0400

    included test covering 100%

--- Java/src/test/java/com/gildedrose/GildedRoseShould.java
+++ Java/src/test/java/com/gildedrose/GildedRoseShould.java
@@ -53,4 +53,41 @@ class GildedRoseShould {
         assertEquals(50, gildedRose.items[0].quality);
     }
 
+    @Test
+    void when_item_is_AgedBrie_and_quality_is_less_than_49_and_sellin_is_less_than_0_then_quality_should_increase_in_1_and_decrease_sellIn_in_1(){
+        Item [] items = {new Item("Aged Brie",-1, 48)};
+
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(50, gildedRose.items[0].quality);
+        assertEquals(-2, gildedRose.items[0].sellIn);
+
+    }
+
+    @Test
+    void when_item_is_Backstage_and_sellIn_is_less_than_0_then_quality_should_be_set_in_0_and_decreases_sellIn_in_1(){
+        Item [] items = {new Item("Backstage passes to a TAFKAL80ETC concert",-1,999)};
+
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(0, gildedRose.items[0].quality);
+        assertEquals(-2, gildedRose.items[0].sellIn);
+    }
+
+    @Test
+    void when_item_is_unknown_and_quality_is_more_than_0_then_should_decrease_quality_in_2_and_decrease_sellIn_in_1(){
+        Item [] items = new Item[]{new Item("Another Different Item", -1, 999)};
+
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(997, gildedRose.items[0].quality);
+        assertEquals(-2, gildedRose.items[0].sellIn);
+    }
+
 }
