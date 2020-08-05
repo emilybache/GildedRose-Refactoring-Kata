6631f5de372a996427ea2775bde7b9674956dfef
Samuel Tran <tran.ksamuel@gmail.com>
2020-08-05 00:57:02 -0500

    Pass initial test for GildedRoseTest.cs

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -11,7 +11,7 @@ namespace csharpcore
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name); 
+            Assert.Equal("foo", Items[0].Name); 
         }
     }
 }
