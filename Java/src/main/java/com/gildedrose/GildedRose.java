31f7d222ea4633897d0342c7242f78e75e797b24
liaolizhen <lizhen.liao@104.com.tw>
2020-08-13 17:43:51 +0800

    調整item.sellIn順序 且將<0的判斷塞入 item.name的判斷式中

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,34 +17,33 @@ class GildedRose {
 		if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
 			return;
 		}
-		if (item.name.equals("Aged Brie")) {
-			increaseQuality(item);
-		} else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-			increaseQuality(item);
-			if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (item.sellIn < 11) {
-					increaseQuality(item);
-				}
-
-				if (item.sellIn < 6) {
-					increaseQuality(item);
-				}
-			}
-		} else {
-			decreaseQuality(item);
-		}
 
 		item.sellIn = item.sellIn - 1;
 
-		if (item.sellIn < 0) {
-			if (item.name.equals("Aged Brie")) {
+		if (item.name.equals("Aged Brie")) {
+			increaseQuality(item);
+			if (item.sellIn < 0) {
 				increaseQuality(item);
-			} else {
-				if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-					item.quality = item.quality - item.quality;
-				} else {
-					decreaseQuality(item);
-				}
+			}
+		} else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+			increaseQuality(item);
+
+			if (item.sellIn < 10) {
+				increaseQuality(item);
+			}
+
+			if (item.sellIn < 5) {
+				increaseQuality(item);
+			}
+
+			if (item.sellIn < 0) {
+				item.quality = item.quality - item.quality;
+			}
+
+		} else {
+			decreaseQuality(item);
+			if (item.sellIn < 0) {
+				decreaseQuality(item);
 			}
 		}
 	}
