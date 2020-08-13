4cfe62665decd97481dbe143233440ede8c2ea22
liaolizhen <lizhen.liao@104.com.tw>
2020-08-13 17:24:42 +0800

    extract increaseQuality & decreaseQuality function

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,47 +17,47 @@ class GildedRose {
 		if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
 			return;
 		}
-
 		if (item.name.equals("Aged Brie")) {
-			if (item.quality < 50) {
-				item.quality = item.quality + 1;
-			}
+			increaseQuality(item);
 		} else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-			if (item.quality < 50) {
-				item.quality = item.quality + 1;
-
+			increaseQuality(item);
+			if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 				if (item.sellIn < 11) {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
+					increaseQuality(item);
 				}
 
 				if (item.sellIn < 6) {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
+					increaseQuality(item);
 				}
 			}
-		} else if (item.quality > 0) {
-			item.quality = item.quality - 1;
+		} else {
+			decreaseQuality(item);
 		}
 
 		item.sellIn = item.sellIn - 1;
 
 		if (item.sellIn < 0) {
 			if (item.name.equals("Aged Brie")) {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
+				increaseQuality(item);
 			} else {
 				if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 					item.quality = item.quality - item.quality;
 				} else {
-					if (item.quality > 0) {
-						item.quality = item.quality - 1;
-					}
+					decreaseQuality(item);
 				}
 			}
+		}
+	}
+
+	public void decreaseQuality(Item item) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
+	}
+
+	public void increaseQuality(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
 		}
 	}
 }
