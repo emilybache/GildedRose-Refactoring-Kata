e00567ab6ad0e14951ae55742f64894bf5b9aaff
jiajun.chou <jiajun.chou@104.com.tw>
2020-08-13 21:35:44 +0800

    [Duplicated Code] extract to function
    for plusOneQuilaty_if_less_than_fifth

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,23 +11,17 @@ class GildedRose {
 
     for (int i = 0; i < items.length; i++) {
       if (items[i].name.equals("Aged Brie")) {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-        }
+        extracted(i);
       } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1;
 
           if (items[i].sellIn < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
+            extracted(i);
           }
 
           if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
+            extracted(i);
           }
         }
       } else {
@@ -42,9 +36,7 @@ class GildedRose {
 
       if (items[i].sellIn < 0) {
         if (items[i].name.equals("Aged Brie")) {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
-          }
+          extracted(i);
         } else {
           if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             items[i].quality = items[i].quality - items[i].quality;
@@ -53,6 +45,12 @@ class GildedRose {
           }
         }
       }
+    }
+  }
+
+  private void extracted(int i) {
+    if (items[i].quality < 50) {
+      items[i].quality = items[i].quality + 1;
     }
   }
 
