4b94f81e2e6fec0adfe1e9890fe3c1ce820a9d16
jiajun.chou <jiajun.chou@104.com.tw>
2020-08-13 21:23:28 +0800

    Format Google Coding style

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -2,20 +2,20 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+  public String name;
 
-    public int sellIn;
+  public int sellIn;
 
-    public int quality;
+  public int quality;
 
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+  public Item(String name, int sellIn, int quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
 
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
+  @Override
+  public String toString() {
+    return this.name + ", " + this.sellIn + ", " + this.quality;
+  }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,16 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+  @Test
+  void foo() {
+    Item[] items = new Item[] {new Item("foo", 0, 0)};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("fixme", app.items[0].name);
+  }
 
 }
