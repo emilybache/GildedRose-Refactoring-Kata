2a60407d6b375091931eb6494065c3226ddebb49
Xavier Levaux <xavier.levaux@gmail.com>
2020-09-24 17:02:46 +0200

    Added tests to ensure no regression is introduced during refactoring

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,26 @@
 package com.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+   @Test
+   void updateQuality() {
+      CombinationApprovals
+          .verifyAllCombinations(this::doUpdateQuality,
+              new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert",
+                  "Sulfuras, Hand of Ragnaros"}, //name
+              new Integer[]{-1, 0, 5, 6, 11}, //sellIn
+              new Integer[]{0, 1, 49, 50} //quantity
+          );
+   }
+
+   private String doUpdateQuality(String name, int sellIn, int quality) {
+      Item[] items = new Item[]{new Item(name, sellIn, quality)};
+      GildedRose app = new GildedRose(items);
+      app.updateQuality();
+      return app.items[0].toString();
+   }
 
 }
