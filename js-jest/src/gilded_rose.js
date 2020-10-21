9cbeedd736a5f93f0a22b3ff5cd2ce5292cf4814
Benjamin Barreto <4544900-benalexb@users.noreply.gitlab.com>
2020-10-21 13:42:37 +0200

    Added starndjs for linting, resolved initial errors, tested inheritance approach to tackle constraint of not touching the Item class

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,67 +1,95 @@
+
+/**
+ * Do not alter the Item class or Items property as those belong to the goblin in the corner who
+ * will insta-rage and one-shot you as he doesn't believe in shared code ownership
+ */
 class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
+  constructor (name, sellIn, quality) {
+    this.name = name
+    this.sellIn = sellIn
+    this.quality = quality
+  }
+}
+
+class Blade extends Item {
+  constructor(...args) {
+    super(...args)
+  }
+
+  whatAmI() {
+    console.log(`[Blade] I am a Blade with: name: ${this.name}, sellIn: ${this.sellIn}, quality: ${this.quality}`)
+  }
+}
+
+class Sword extends Blade {
+  constructor(...args) {
+    super(...args)
+  }
+
+  whoAmI() {
+    console.log('[Sword] I am a sword! AND...')
+    this.whatAmI()
   }
 }
 
 class Shop {
-  constructor(items=[]){
-    this.items = items;
+  constructor (items = []) {
+    this.items = items
   }
-  updateQuality() {
+
+  updateQuality () {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name !== 'Aged Brie' && this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+          if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.items[i].quality - 1
           }
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          this.items[i].quality = this.items[i].quality + 1
+          if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality = this.items[i].quality + 1
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality = this.items[i].quality + 1
               }
             }
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].sellIn = this.items[i].sellIn - 1
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name !== 'Aged Brie') {
+          if (this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+              if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].quality = this.items[i].quality - 1
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality = this.items[i].quality - this.items[i].quality
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.items[i].quality = this.items[i].quality + 1
           }
         }
       }
     }
 
-    return this.items;
+    return this.items
   }
 }
 
 module.exports = {
   Item,
-  Shop
+  Shop,
+  Sword
 }
