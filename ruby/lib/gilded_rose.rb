2b410aa50bf4c302e261aee1aee3da9138c3a86d
ollie beney <ombeney@gmail.com>
2020-11-04 15:28:47 +0000

    refactor backstage path to use regex, this allows other backstage items to work provided backstage is in their name

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -15,7 +15,7 @@ class GildedRose
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.name.downcase.match /backstage/
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -34,7 +34,7 @@ class GildedRose
       end
       if item.sell_in < 0
         if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if !item.name.downcase.match /backstage/
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
