92b6f1df1573378a9319de536cc3e6709e222254
Dan Holmes <dh@danielholmes.com>
2020-12-03 11:52:38 +0000

    Fix and pass first test

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,10 +1,10 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+var { Shop, Item } = require('../src/gilded_rose.js');
+describe("Gilded Rose", function () {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  it("should foo", function () {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
 
 });
