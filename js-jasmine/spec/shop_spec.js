4639d18db0241390760bbb12654a315b3936dbbb
Dan Holmes <dh@danielholmes.com>
2020-12-03 13:33:57 +0000

    Add test: decreases quality by 1

--- js-jasmine/spec/shop_spec.js
+++ js-jasmine/spec/shop_spec.js
@@ -1,27 +1,30 @@
 var { Shop } = require('../src/shop.js');
 
-describe(Shop, () => {
+describe('Shop', () => {
   const item = {
     name: 'foo',
     sellIn: 5,
     quality: 5
-  }
-  beforeEach(() => {
-    item.name = 'foo';
-    item.sellIn = 5;
-    item.quality = 5;
-  })
+  };
   const gildedRose = new Shop([item]);
+  let items = []
   describe('.updateQuality', () => {
     it("keeps name the same", () => {
-      const items = gildedRose.updateQuality();
+      items = gildedRose.updateQuality();
       expect(items[0].name).toEqual("foo");
     });
     describe('when sellIn and quality are above 0', () => {
+      beforeEach(() => {
+        item.sellIn = 5;
+        item.quality = 5;
+        items = gildedRose.updateQuality();
+      });
       it('descreases sellIn by 1', () => {
-        const items = gildedRose.updateQuality();
         expect(items[0].sellIn).toEqual(4);
       });
+      it('decreases quality by 1', () => {
+        expect(items[0].sellIn).toEqual(4);
+      })
     });
   });
 });
