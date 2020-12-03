e72f1de3afa5846204a0d6a6219d83a19e480663
Dan Holmes <dh@danielholmes.com>
2020-12-03 12:25:05 +0000

    Add test: decreases sellIn by 1

--- js-jasmine/spec/shop_spec.js
+++ js-jasmine/spec/shop_spec.js
@@ -1,16 +1,27 @@
 var { Shop } = require('../src/shop.js');
 
-describe(Shop, function () {
-  let item = {
+describe(Shop, () => {
+  const item = {
     name: 'foo',
     sellIn: 5,
     quality: 5
   }
-  describe('.updateQuality', function () {
-    it("keeps name the same", function () {
-      const gildedRose = new Shop([item]);
+  beforeEach(() => {
+    item.name = 'foo';
+    item.sellIn = 5;
+    item.quality = 5;
+  })
+  const gildedRose = new Shop([item]);
+  describe('.updateQuality', () => {
+    it("keeps name the same", () => {
       const items = gildedRose.updateQuality();
       expect(items[0].name).toEqual("foo");
+    });
+    describe('when sellIn and quality are above 0', () => {
+      it('descreases sellIn by 1', () => {
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(4);
+      });
     });
   });
 });
