7767e6a6f6ac72f6924e4ab2e3179a66b2a8baf1
Feken Baboyan <feken.baboyan@shopify.com>
2021-02-25 14:51:18 -0500

    Move ItemFactory to a separate file

---
+++ ruby/item_factory.rb
@@ -0,0 +1,10 @@
+class ItemFactory
+  SPECIAL_ITEMS = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"]
+  def self.create_item(name:, sell_in:, quality:)
+    if SPECIAL_ITEMS.include?(name)
+      return Item.new(name, sell_in, quality)
+    end
+
+    GenericItem.new(name, sell_in, quality)
+  end
+end
