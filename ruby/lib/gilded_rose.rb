dc9e8be57d6ddcd613da213d40abd5f7f72f1987
Feken Baboyan <feken.baboyan@shopify.com>
2021-02-25 15:24:12 -0500

    Restructure GildedRose Project

---
+++ ruby/lib/gilded_rose.rb
@@ -0,0 +1,5 @@
+require File.join(File.dirname(__FILE__), "gilded_rose/store" )
+require File.join(File.dirname(__FILE__), "gilded_rose/item_factory" )
+
+module GildedRose
+end
---
+++ ruby/tests/gilded_rose/item_factory_tests.rb
@@ -0,0 +1,19 @@
+require_relative '../../lib/gilded_rose'
+require 'test/unit'
+
+module GildedRose
+  class ItemFactoryTests < Test::Unit::TestCase
+    test ".create_item returns GenericItem instance if it is not a special item" do
+      created_item = ItemFactory.create_item(name: "random_item", sell_in: 0, quality: 0)
+  
+      assert_instance_of GenericItem, created_item
+    end
+  
+    test ".create_item returns Item instance if Aged Brie item if given" do
+      created_item = ItemFactory.create_item(name: "Aged Brie", sell_in: 0, quality: 0)
+  
+      assert_instance_of Item, created_item
+    end
+  end
+end
+
