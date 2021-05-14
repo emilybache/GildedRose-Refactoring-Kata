9e0fbcb360edf9edf7f83c7e3e88bec747433885
degwinthegreat <the.colledge.dropout@gmail.com>
2021-05-14 21:03:34 +0900

    第一部 完

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -11,8 +11,33 @@ class GildedRose
     @items = items
   end
 
-  def increment_item_quality(item, num = 1)
-    item.quality = [item.quality + num, QUALITY_THRESHOLD].min
+  # - AGED_BRIEは時間とともに品質が上がり、販売期限が過ぎるとさらに品質が上がる
+  # - SULFURASについては何もしない
+  # - 通常のitemは徐々に品質が下がり、販売期限が下がるとさらに品質が下がる
+  # - アイテムの品質は50まで
+  # - item quority は 0以下にはならないk
+  # - BACKSTAGE_PASSは、sell in  11 以上のときは + 1
+  # - BACKSTAGE_PASSは、sell in  6-10のときは + 2
+  # - BACKSTAGE_PASSは、sell in  6 以下のときは + 3
+
+  def update_quality()
+    @items.each do |item|
+      next if item.name == SULFURAS
+
+      first_step(item)
+      item.sell_in = item.sell_in - 1
+      next if item.sell_in >= 0
+      case item.name
+      when AGED_BRIE
+        increment_item_quality(item)
+      when BACKSTAGE_PASS
+          item.quality = 0
+      else
+        if item.quality > 0
+          item.quality = item.quality - 1
+        end
+      end
+    end
   end
 
   def first_step(item)
@@ -33,24 +58,8 @@ class GildedRose
     end
   end
 
-  def update_quality()
-    @items.each do |item|
-      next if item.name == SULFURAS
-
-      first_step(item)
-      item.sell_in = item.sell_in - 1
-      next if item.sell_in >= 0
-      case item.name
-      when AGED_BRIE
-        increment_item_quality(item)
-      when BACKSTAGE_PASS
-          item.quality = 0
-      else
-        if item.quality > 0
-          item.quality = item.quality - 1
-        end
-      end
-    end
+  def increment_item_quality(item, num = 1)
+    item.quality = [item.quality + num, QUALITY_THRESHOLD].min
   end
 end
 
