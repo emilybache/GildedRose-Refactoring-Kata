eaa731bb835370f08ebb8df2d4acbe3ad9e61fea
Peter M. Clausen <petermichaelclausen@gmail.com>
2021-05-25 19:52:50 +0200

    First commit of Fortran version of GildedRose

---
+++ fortran/test/GildedRose_unity_test.f90
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+program GildedRose_texttest
+  use GildedRose
+  implicit none
+  type(Item) :: it
+
+  call init_item(it, "foo", 10, 20)
+  if (trim(it%name) /= "foo" ) then
+    write(*,*) "ERRORUnity test Failed"
+    stop 1
+  else
+    write(*,*) "Unity test OK"
+  endif
+
+end program
