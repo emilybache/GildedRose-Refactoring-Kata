ba464f571477cdad328a946050b9938d447e239e
Ben Hemann <benhemann@yahoo.com>
2021-06-04 16:31:44 -0500

    refactored first test to pass

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -6,7 +6,7 @@ describe('Gilded Rose', function () {
     it('should foo', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].name).to.equal('foo');
     });
 
 });
