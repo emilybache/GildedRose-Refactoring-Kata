8bb15c58dc645954e3fc46d79569a31f5b2b20c4
Lewie Mortier <lewiemortier@gmail.com>
2021-06-22 20:47:12 +0200

    First unit tests + update directory for ci

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,14 +6,64 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+    internal fun `regular item decreases as expected`() {
+        val items = arrayOf(Item("foo", 1, 1))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
 
+        app.assertQualityAndSellIn(
+                expectedSellIn = 0,
+                expectedQuality = 0
+        )
     }
 
+    @Test
+    internal fun `Quality decreases twice as fast after expiry date passed`() {
+        val item = Item("foo", 0, 2)
+        val app = GildedRose(arrayOf(item))
+        app.updateQuality()
+
+        app.assertQualityAndSellIn(
+                expectedSellIn = -1,
+                expectedQuality = 0
+        )
+    }
+
+    @Test
+    internal fun `Quality decreases normally when reaching sellIn date`() {
+        val item = Item("foo", 1, 5)
+        val app = GildedRose(arrayOf(item))
+        app.updateQuality()
+
+        app.assertQualityAndSellIn(
+                expectedSellIn = 0,
+                expectedQuality = 4
+        )
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = -1,
+                expectedQuality = 2
+        )
+    }
+
+    @Test
+    internal fun `Quality decreases after sellin reached 0 and quality is 0`() {
+        val item = Item("foo", 0, 2)
+        val app = GildedRose(arrayOf(item))
+        app.updateQuality()
+
+        app.assertQualityAndSellIn(
+                expectedSellIn = -1,
+                expectedQuality = 0
+        )
+    }
+
+    private fun GildedRose.assertQualityAndSellIn(expectedSellIn: Int, expectedQuality: Int, itemIndex: Int = 0) {
+        val item = items[itemIndex]
+        assertEquals(expectedSellIn, item.sellIn)
+        assertEquals(expectedQuality, item.quality)
+    }
 }
 
 
