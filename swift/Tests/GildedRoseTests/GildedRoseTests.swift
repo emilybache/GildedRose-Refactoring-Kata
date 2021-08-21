f26e495403260e1412cdf2799281e413b7abf4c5
Jon Reid <jon@qualitycoding.org>
2021-08-20 21:27:52 -0700

    t Define test as throws

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class GildedRoseTests: XCTestCase {
 
-    func testFoo() {
+    func testFoo() throws {
         let items = [Item(name: "foo", sellIn: 0, quality: 0)]
         let app = GildedRose(items: items)
         app.updateQuality()
