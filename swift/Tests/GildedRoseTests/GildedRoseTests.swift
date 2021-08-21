31825b24107793ee73af0144b62b8012c33d682f
Jon Reid <jon@qualitycoding.org>
2021-08-20 21:27:23 -0700

    t Remove semicolons from test

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -5,8 +5,8 @@ class GildedRoseTests: XCTestCase {
 
     func testFoo() {
         let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-        let app = GildedRose(items: items);
-        app.updateQuality();
+        let app = GildedRose(items: items)
+        app.updateQuality()
         XCTAssertEqual("fixme", app.items[0].name);
     }
 
