494935de66103adb76fd848e41ff3ffeb8204c31
jerem103 <89345388+jerem103@users.noreply.github.com>
2021-08-22 20:19:35 +0200

    Update GildedRose.java
    add feature for Conjured items

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,6 +15,9 @@ class GildedRose {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
+                    if (items[i].name.equals("Conjured Mana Cake") && items[i].quality > 0) {
+                        items[i].quality = items[i].quality - 1;
+                    }
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -45,6 +48,9 @@ class GildedRose {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                                items[i].quality = items[i].quality - 1;
+                            }
+                            if (items[i].name.equals("Conjured Mana Cake")  && items[i].quality > 0) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
