a15e48a03eea6ae189d2d48612ce38f12afc579c
Adefolarin Adeniji <adefolarin.adeniji@gmail.com>
2021-08-28 11:33:46 +0100

    Modified files
    - Deleted UpdateQuality and UpdateSellin in GildedRose
    - Modified BackStagePasses

--- csharpcore/GildedRose/Models/BackStagePasses.cs
+++ csharpcore/GildedRose/Models/BackStagePasses.cs
@@ -12,18 +12,21 @@ namespace GildedRose.Models
             if (this.SellDaysGone > this.SellIn)
             {
                 this.Quality = 0;
-            }
-
-            if ((this.SellIn - this.SellDaysGone) <= 5)
-            {
-                if (this.Quality < 51)
-                    this.Quality += 3;
+                return;
             }
 
             if ((this.SellIn - this.SellDaysGone) <= 10)
             {
                 if (this.Quality < 51)
                     this.Quality += 2;
+                return;
+            }
+
+            if ((this.SellIn - this.SellDaysGone) <= 5)
+            {
+                if (this.Quality < 51)
+                    this.Quality += 3;
+                return;
             }
         }
 
