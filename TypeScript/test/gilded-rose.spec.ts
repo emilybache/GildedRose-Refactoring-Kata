e318f82ebb7f0f2d87a161222ea8854f73c1163a
thiwanka-aux <47382911+thiwanka-aux@users.noreply.github.com>
2021-09-20 14:02:14 +0530

    Added tc : quality should degrades quality by 1

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -3,10 +3,22 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
+    // quality of the normal items degrades by 1
+    it('quality should degrades quality by 1', function() {
+        const dataset = [
+            new Item('item 1', 31, 12),
+            new Item('item 2', 45, 21),
+        ]
 
+        const gildedRose = new GildedRose(dataset);
+        const result = gildedRose.updateQuality();
+
+        expect(result[0].name).to.equal('item 1');
+        expect(result[0].sellIn).to.equal(30);
+        expect(result[0].quality).to.equal(11);
+
+        expect(result[1].name).to.equal('item 2');
+        expect(result[1].sellIn).to.equal(44);
+        expect(result[1].quality).to.equal(20);
+    });
 });
