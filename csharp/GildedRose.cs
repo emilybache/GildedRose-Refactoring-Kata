c262bbb922cf68e485c7177879a289a85d66522c
ieZaky <McEslam58@gmail.com>
2021-10-31 11:29:33 +0200

    Added new changes regarding Conjured items and refactored some code structure

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace csharp
 {
     public class GildedRose
     {
         IList<Item> Items;
+        public const int sulfurasMaxQuality = 80;
+        public const int itemMaxQuality = 50;
+        public const int qualityScore = 50;
+        public const int daysToDoublePassesQuantity = 11;
+        public const int daysToTriplePassesQuantity = 6;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -14,73 +20,56 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                // Updating Quality of items
+
+                if(Items[i].Name == "Aged Brie" && Items[i].Quality < itemMaxQuality)
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
+                    Items[i].Quality = Items[i].Quality + 1;
                 }
-                else
+                else if(Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].Quality < itemMaxQuality)
                 {
-                    if (Items[i].Quality < 50)
+
+                    if (Items[i].SellIn < daysToTriplePassesQuantity)
+                    {
+                        Items[i].Quality = Items[i].Quality + 2;
+                    }
+
+                    else if (Items[i].SellIn < daysToDoublePassesQuantity)
                     {
                         Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
                     }
+                    
+                }
+                else if(Items[i].Name == "Conjured Mana Cake" && Items[i].Quality > 0)
+                {
+                    Items[i].Quality = Items[i].Quality - 2;
+                }
+                else  if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                        Items[i].Quality = Items[i].Quality - 1;
                 }
 
+
+                // Selling Date
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
+                // Selling Date has passed
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                   if(Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
+                            Items[i].Quality = 0;
+                    }  
+                   else if (Items[i].Name == "Aged Brie" && Items[i].Quality < itemMaxQuality)
                     {
-                        if (Items[i].Quality < 50)
-                        {
                             Items[i].Quality = Items[i].Quality + 1;
-                        }
+                    }
+                    else if(Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        Items[i].Quality = Items[i].Quality - 1;
                     }
                 }
             }
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -9,7 +9,7 @@ namespace csharp
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
+            List<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
