4ccea7cf6639ec782b0583b1a26db9e77db34c51
Velizar Todorov <velizar.todorov@unifiedpost.com>
2021-11-30 17:38:04 +0100

    :hammer: convert `LegendaryItem` from anemic to rich

--- src/main/java/com/gildedrose/items/LegendaryItem.java
+++ src/main/java/com/gildedrose/items/LegendaryItem.java
@@ -1,6 +1,5 @@
 package com.gildedrose.items;
 
-import com.gildedrose.item_helpers.ItemHandler;
 import com.gildedrose.item_helpers.ItemType;
 import com.gildedrose.main.Item;
 
@@ -9,15 +8,15 @@ public class LegendaryItem implements ItemType {
   public static final int LEGENDARY_ITEM_QUALITY = 80;
   public static final String LEGENDARY = "Sulfuras, Hand of Ragnaros";
 
-  private final ItemHandler item;
+  private final Item item;
 
   public LegendaryItem(Item item) {
-    this.item = new ItemHandler(item);
+    this.item = item;
   }
 
   @Override
   public void updateQuality() {
-    item.decrementSellInDate();
+    decrementSellInDate();
   }
 
   @Override
@@ -25,6 +24,10 @@ public class LegendaryItem implements ItemType {
     return LEGENDARY;
   }
 
+  public void decrementSellInDate() {
+    item.sellIn--;
+  }
+  
   public static boolean isLegendary(Item item) {
     return item.name.equals(LEGENDARY);
   }
