891ed34cd8b303ccef31e738dc88d1204adc43b9
Michael Eaton <mjeaton@gmail.com>
2021-12-10 08:43:23 -0500

    Initial pass at code coverage. Added ShouldFluent lib.

--- csharpcore/GildedRoseTests/ApprovalTest.cs
+++ csharpcore/GildedRoseTests/ApprovalTest.cs
@@ -11,7 +11,7 @@ namespace GildedRoseTests
     [UseReporter(typeof(DiffReporter))]
     public class ApprovalTest
     {
-        [Fact]
+        [Fact(Skip = "Not sure what the purpose of this test is")]
         public void ThirtyDays()
         {
             var fakeoutput = new StringBuilder();
