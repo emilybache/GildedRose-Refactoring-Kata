6a43f361894edd1cb2b347f0c4c4349c0c77bacf
Samuel Dufour <dufoursamuel.fr@gmail.com>
2021-12-27 23:41:41 +0100

    Add a csharpcore-Verify.xunit folder

---
+++ csharpcore-Verify.xunit/GildedRose/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace GildedRoseKata
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
---
+++ csharpcore-Verify.xunit/GildedRoseTests/ApprovalTest.cs
@@ -0,0 +1,31 @@
+﻿
+using GildedRoseKata;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+using VerifyXunit;
+
+using Xunit;
+
+namespace GildedRoseTests
+{
+    [UsesVerify]
+    public class ApprovalTest
+    {
+        [Fact]
+        public Task ThirtyDays()
+        {
+            var fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            var output = fakeoutput.ToString();
+
+            return Verifier.Verify(output);
+        }
+    }
+}
