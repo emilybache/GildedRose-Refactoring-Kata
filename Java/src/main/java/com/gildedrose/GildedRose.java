9d53036564b62aabb37ee4d0080d48cb7c579955
Alpi-Laptop <a.inceismail@tourbosoft.de>
2022-01-16 12:59:51 +0100

    #added conjured

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,10 +10,18 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+                        if (items[i].name.startsWith("Conjured")) {
+                            if (items[i].quality == 1) {
+                                items[i].quality = 0;
+                            } else {
+                                items[i].quality = items[i].quality - 2;
+                            }
+                        } else {
+                            items[i].quality = items[i].quality - 1;
+                        }
                     }
                 }
             } else {
@@ -45,7 +53,15 @@ class GildedRose {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+                                if (items[i].name.startsWith("Conjured")) {
+                                    if (items[i].quality == 1) {
+                                        items[i].quality = 0;
+                                    } else {
+                                        items[i].quality = items[i].quality - 2;
+                                    }
+                                } else {
+                                    items[i].quality = items[i].quality - 1;
+                                }
                             }
                         }
                     } else {
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,25 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
 
+    //    public Item(String name, int sellIn, int quality) {
+    @Test
+    void conjuredItemQualityDecreaseTwice() {
+        Item[] items = new Item[]{new Item("Conjured Cake", 0, 2), new Item("Aged Brie", 1, 30)};
+        GildedRose glidedRose = new GildedRose(items);
+        glidedRose.updateQuality();
+        assertEquals(0, glidedRose.items[0].quality);
+    }
+    @Test
+    void conjuredItemQualityShouldotBeNegative() {
+        Item[] items = new Item[]{new Item("Conjured Cake", 0, 1), new Item("Aged Brie", 1, 30)};
+        GildedRose glidedRose = new GildedRose(items);
+        glidedRose.updateQuality();
+        assertEquals(0, glidedRose.items[0].quality);
+    }
 }
