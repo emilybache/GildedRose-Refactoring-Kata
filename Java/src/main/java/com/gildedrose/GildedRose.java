16e47099d566c40985cdc9400f26fdb58149e1ca
Renikunta Pradeep <47050942+rpradeepkbi007@users.noreply.github.com>
2022-03-15 14:24:12 +0530

    Update GildedRose.java
    added condition : "Conjured" items degrade in Quality twice as fast as normal items.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,13 +10,16 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { 
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
+                    if(items[i].name.equals("Conjured")) { // reducing twice for conjured items, condition["Conjured" items degrade in Quality twice as fast as normal items]
+                        items[i].quality = items[i].quality - 1;
+                    }
                 }
-            } else {
+            } else { 
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
@@ -36,22 +39,25 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { 
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
+            if (items[i].sellIn < 0) { 
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
+                            if(items[i].name.equals("Conjured")) { // reducing twice for conjured items if sellIn less than zero, condition [Once the sell by date has passed, Quality degrades twice as fast]
+                                items[i].quality = items[i].quality - 1;
+                            }
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
+                } else { 
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
