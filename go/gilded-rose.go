38264396abee7e1552b1ef89d7f0efe84df6209b
richard stagg <richard.s.stagg@oracle.com>
2022-03-28 16:33:43 +0100

    Corrected Conjured name

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -11,7 +11,7 @@ func UpdateQuality(items []*Item) {
 		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
 			if items[i].quality > 0 {
 				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					if items[i].name == "Conjured" {
+					if items[i].name == "Conjured Mana Cake" {
 						items[i].quality = items[i].quality - 2
 						if items[i].quality < 0 {items[i].quality=0}
 					} else {
