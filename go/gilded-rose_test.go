e1c6ab2187250ee32da8e456a50c735aed0ea18b
richard stagg <richard.s.stagg@oracle.com>
2022-03-28 16:59:06 +0100

    Add a quick test for the new code

--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -1,15 +1,23 @@
 package main
 
-import "testing"
+import ("testing" 
+        "fmt")
 
 func Test_Foo(t *testing.T) {
 	var items = []*Item{
-		&Item{"fixme", 1, 10},
+		&Item{"Conjured Mana Cake", 1, 10},
 	}
 
-	UpdateQuality(items)
+	days := 2
+	for day := 0; day < days; day++ {
+	    UpdateQuality(items)
+		fmt.Println(items[0])
+	}
 
-	if items[0].name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].name)
+	if items[0].name != "Conjured Mana Cake" {
+		t.Errorf("Name: Expected %s but got %s ", "Conjured Mana Cake", items[0].name)
+	}
+	if items[0].quality != 4 {
+		t.Errorf("Quality:  Expected %s but got %d","4",items[0].quality)
 	}
 }
