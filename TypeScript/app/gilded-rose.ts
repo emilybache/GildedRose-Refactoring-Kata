7c81b6d242dd6988db48bb2154a1b58d2a27996b
Jesper <jesper@sanalabs.com>
2022-04-13 16:39:01 +0200

    Invert agedbrie and backstage conditional

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,13 +19,7 @@ export class GildedRose {
 
   updateQuality() {
     this.items.forEach((item, i) => {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
-          }
-        }
-      } else {
+      if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality < 50) {
           item.quality = item.quality + 1
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -39,6 +33,12 @@ export class GildedRose {
                 item.quality = item.quality + 1
               }
             }
+          }
+        }
+      } else {
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality = item.quality - 1
           }
         }
       }
