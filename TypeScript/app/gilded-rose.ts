e734b35f3a1f35be08da6a2c5c485acc630b7718
Jesper <jesper@sanalabs.com>
2022-04-13 16:27:20 +0200

    Extract sellInBelow0

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -46,24 +46,28 @@ export class GildedRose {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
-        }
+        this.sellInBelow0(item);
       }
     })
 
     return this.items;
+  }
+
+  private sellInBelow0(item: Item) {
+    if (item.name != 'Aged Brie') {
+      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality = item.quality - 1;
+          }
+        }
+      } else {
+        item.quality = item.quality - item.quality;
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    }
   }
 }
