4f222d34810af4ee57372599b7cf95c117a18ee5
shanmukhanath <shan_nath_b@yahoo.com>
2022-06-12 16:35:38 +0530

    Flattens if conditions

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,33 +15,33 @@ class GildedRose(object):
             self.update_item_quality(item)
 
     def update_item_quality(self, item):
+        isExpired = item.sell_in < 0
+        doesDegradeQaulity = item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES and item.name != self.SULFURAS
         
-        if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
-            if item.quality > 0:
-                
-                if item.name != self.SULFURAS:
-                    self.adjust_quality(item, - self.qualityIncrease)
-        else:
-            if item.quality < 50:
+        if doesDegradeQaulity:
+            self.adjust_quality(item, - self.qualityIncrease)
+        
+        if item.name == self.AGED_BRIE:
+            self.adjust_quality(item, self.qualityIncrease)
+            
+        if item.name == self.BACKSTAGE_PASSES:
+            self.adjust_quality(item, self.qualityIncrease)
+            if item.sell_in < 11:
                 self.adjust_quality(item, self.qualityIncrease)
-                if item.name == self.BACKSTAGE_PASSES:
-                    if item.sell_in < 11:
-                        self.adjust_quality(item, self.qualityIncrease)
-                    if item.sell_in < 6:
-                        self.adjust_quality(item, self.qualityIncrease)
+            if item.sell_in < 6:
+                self.adjust_quality(item, self.qualityIncrease)
+        
         if item.name != self.SULFURAS:
             item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.name != self.AGED_BRIE:
-                if item.name != self.BACKSTAGE_PASSES:
-                    if item.quality > 0:
-                        if item.name != self.SULFURAS:
-                            self.adjust_quality(item, - self.qualityIncrease)
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                if item.quality < 50:
-                    self.adjust_quality(item, self.qualityIncrease)
+        
+        
+        if isExpired:
+            if doesDegradeQaulity:
+                self.adjust_quality(item, - self.qualityIncrease)
+            if item.quality == self.BACKSTAGE_PASSES:
+                item.quality = item.quality - item.quality
+            if item.quality == self.AGED_BRIE:
+                self.adjust_quality(item, self.qualityIncrease)
 
     # Update the quality item value if the quality value is within the range 0 to 50
     def adjust_quality(self, item, quality_increase_decrease):
@@ -51,7 +51,7 @@ class GildedRose(object):
 
 
 
-class Item:
+class Item:  
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
