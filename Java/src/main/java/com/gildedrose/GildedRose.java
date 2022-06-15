81c54bcb7a14b479e69fe844783219acb06c7668
subho <>
2022-06-15 08:36:07 +0530

    Updated as per requirement

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -22,18 +22,22 @@ class GildedRose {
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                            if (items[i].quality + 2 <= 50) {
+                                items[i].quality = items[i].quality + 2;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                            if (items[i].quality+3 <= 50) {
+                                items[i].quality = items[i].quality + 3;
                             }
                         }
                     }
                 }
+            }
+
+            if(items[i].name.equals("Conjured") && items[i].quality > 1){
+                items[i].quality = items[i].quality - 2;
             }
 
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
