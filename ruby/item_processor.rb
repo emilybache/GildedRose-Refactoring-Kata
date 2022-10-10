ae84163e48112ea9e5cd9483b5a5e1f01ebd772c
davidraj <davidraj.samuel@gmail.com>
2022-10-10 16:53:06 +0100

    Assignment of operators

--- ruby/item_processor.rb
+++ ruby/item_processor.rb
@@ -10,19 +10,17 @@ class ItemProcessor
   def update_item_quality
     if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
       if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality = item.quality - 1
-        end
+        item.quality -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
       end
     else
       if item.quality < 50
-        item.quality = item.quality + 1
+        item.quality += 1
         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
           if item.sell_in < 11
-            item.quality = item.quality + 1 if item.quality < 50
+            item.quality += 1 if item.quality < 50
           end
           if item.sell_in < 6
-            item.quality = item.quality + 1 if item.quality < 50
+            item.quality += 1 if item.quality < 50
           end
         end
       end
@@ -32,15 +30,13 @@ class ItemProcessor
       if item.name != 'Aged Brie'
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
           if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality = item.quality - 1
-            end
+            item.quality -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
           end
         else
           item.quality = item.quality - item.quality
         end
       else
-        item.quality = item.quality + 1 if item.quality < 50
+        item.quality += 1 if item.quality < 50
       end
     end
   end
