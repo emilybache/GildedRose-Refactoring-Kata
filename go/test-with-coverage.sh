760fa27d4a53f420e5583aa2c7dc521ce50f235f
ulf <uivraeus@gmail.com>
2022-10-16 15:25:44 +0200

    Fix python ref in go's test-with-coverage.sh

--- go/test-with-coverage.sh
+++ go/test-with-coverage.sh
@@ -4,4 +4,4 @@ set -e
 go test ./... -coverprofile=coverage/coverage.out
 go tool cover -html=coverage/coverage.out -o coverage/index.html
 echo "Starting web server to present the coverage report. Visit at http://localhost:8000. Press 'Ctrl' + 'C' to exit."
-python -m http.server 8000 --bind 127.0.0.1 --directory coverage 2>&1 | grep -v "GET /.* 200 -"
+python3 -m http.server 8000 --bind 127.0.0.1 --directory coverage 2>&1 | grep -v "GET /.* 200 -"
