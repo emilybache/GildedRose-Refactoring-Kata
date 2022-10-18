672885c269e6eec508223ba5765fb641597a7a64
Ziemowit Kustra <zkustra1997@gmail.com>
2022-10-18 23:22:42 +0200

    removing not needed test cases

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -8,6 +8,7 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
+        //to update
         public void foo()
         {
             List<IItem> Items = new List<IItem> { new Item("foo",0,0)};
@@ -15,43 +16,7 @@ namespace csharp
             app.UpdateQuality();
             //Assert.AreEqual("fixme", Items[0].Name);
         }
-        /*
-        [Test]
-        public void ConjuredFood_SellInGreaterThanOrSameAs1_QualityLoweredBy2()
-        {
-            Arrange
-            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Meat", SellIn = 1, Quality = 3 },
-                                                 new Item { Name = "conjured Fish", SellIn = 2, Quality = 3 },
-                                                 new Item { Name = "Conjured Water", SellIn = 1, Quality = 0 },
-                                                 new Item { Name = "Pie Conjured", SellIn = 1, Quality = 1 }};
-            GildedRose app = new GildedRose(Items);
 
-            Act
-            app.UpdateQuality();
-
-            Assert
-            Assert.AreEqual(1, Items[0].Quality);
-            Assert.AreEqual(1, Items[1].Quality);
-            Assert.AreEqual(0, Items[2].Quality);
-            Assert.AreEqual(0, Items[3].Quality);
-        }
-
-        [Test]
-        public void ConjuredFood_SellInLessThanOrSameAs0_QualityLowerBy4()
-        {
-            Arrange
-            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Meat", SellIn = -1, Quality = 3 },
-                                                 new Item { Name = "conjured Fish", SellIn = 0, Quality = 5 } };
-            GildedRose app = new GildedRose(Items);
-
-            Act
-            app.UpdateQuality();
-
-            Assert
-            Assert.AreEqual(0, Items[0].Quality);
-            Assert.AreEqual(1, Items[1].Quality);
-        }
-        */
         [TestCase("meat", 1, 3, ExpectedResult = 1)]
         [TestCase("meat", 1, 1, ExpectedResult = 0)]
         [TestCase("meat", 0, 3, ExpectedResult = 0)]
