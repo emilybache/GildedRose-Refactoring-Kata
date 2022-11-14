fcdbd79e23d4d89a1d2cdf3dc519d41c448ceebe
Christos Giannouris <chris1giann1@gmail.com>
2022-11-14 13:31:43 +0100

    fix test

--- TypeScript/test/mocha/gilded-rose.spec.ts
+++ TypeScript/test/mocha/gilded-rose.spec.ts
@@ -1,10 +1,10 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '@/gilded-rose';
+import { expect } from "chai";
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe("Gilded Rose", () => {
+  it("should foo", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+    expect(items[0].name).to.equal("foo");
   });
 });
