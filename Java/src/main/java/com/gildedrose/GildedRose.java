aefb4310a9f1e115a8dc514068190cc507a66df3
Trobax <zakariahammoud28@gmail.com>
2022-11-23 16:46:46 +0100

    break down the method
    breaking down the method updateQuality for to various scopes as a refactoring

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,53 +7,61 @@ class GildedRose {
         this.items = items;
     }
 
-    public void updateQuality() {
+    public void updateInventory() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality--;
+            updateItem(item);
+        }
+    }
+
+    private static void updateItem(Item item) {
+        updateQuality(item);
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn--;
+        }
+
+        if (item.sellIn < 0) {
+            if (!item.name.equals("Aged Brie")) {
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality--;
+                        }
                     }
+                } else {
+                    item.quality = 0;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality++;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
-                        }
-                    }
                 }
             }
+        }
+    }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn--;
+    private static void updateQuality(Item item) {
+        if (!item.name.equals("Aged Brie")
+            && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality--;
+                }
             }
+        } else {
+            if (item.quality < 50) {
+                item.quality++;
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality--;
-                            }
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality++;
                         }
-                    } else {
-                        item.quality = 0;
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality++;
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality++;
+                        }
                     }
                 }
             }
--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -30,7 +30,7 @@ public class TexttestFixture {
                 System.out.println(item);
             }
             System.out.println();
-            app.updateQuality();
+            app.updateInventory();
         }
     }
 
