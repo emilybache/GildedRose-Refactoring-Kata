0dca9b86e1e8810191fda5a36403e67408904cd8
Richard Xu <richard.xu@roadbotics.com>
2022-12-22 16:58:29 -0500

    Add item handler factory

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,49 +1,20 @@
 package com.gildedrose;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 class GildedRose {
-  private static final Map<ItemType, Class<? extends ItemHandler>> ITEM_HANDLER_MAP = Map.of(
-    ItemType.AGED_BRIE,
-    AgedBrieItemHandler.class,
-    ItemType.SULFURAS,
-    SulfurasItemHandler.class,
-    ItemType.BACKSTAGE_PASSES,
-    BackstagePassesItemHandler.class,
-    ItemType.CONJURED,
-    ConjuredItemHandler.class,
-    ItemType.GENERIC,
-    GenericItemHandler.class
-  );
-  // make package-private
-  public final List<Item> items;
+  final List<Item> items;
 
   public GildedRose(Item[] items) {
     this.items = Arrays.asList(items);
   }
 
-  // todo: refactor this to use Guice
-  private static void handleDay(Item item) {
-    try {
-      ItemHandler itemHandler = ITEM_HANDLER_MAP
-        .get(ItemType.forDisplayName(item.name))
-        .getDeclaredConstructor()
-        .newInstance();
-      itemHandler.handleDay(item);
-    } catch (
-      InstantiationException
-      | IllegalAccessException
-      | InvocationTargetException
-      | NoSuchMethodException e
-    ) {
-      throw new RuntimeException(e);
-    }
+  public void updateQuality() {
+    items.forEach(this::handleDay);
   }
 
-  public void updateQuality() {
-    items.forEach(GildedRose::handleDay);
+  private void handleDay(Item item) {
+    ItemHandlerFactory.getItemHandler(item).handleDay(item);
   }
 }
---
+++ Java/src/main/java/com/gildedrose/ItemHandlerFactory.java
@@ -0,0 +1,37 @@
+package com.gildedrose;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+public class ItemHandlerFactory {
+
+  private static final Map<ItemType, Class<? extends ItemHandler>> ITEM_HANDLER_MAP = Map.of(
+    ItemType.AGED_BRIE,
+    AgedBrieItemHandler.class,
+    ItemType.SULFURAS,
+    SulfurasItemHandler.class,
+    ItemType.BACKSTAGE_PASSES,
+    BackstagePassesItemHandler.class,
+    ItemType.CONJURED,
+    ConjuredItemHandler.class,
+    ItemType.GENERIC,
+    GenericItemHandler.class
+  );
+
+  // todo: use guice
+  public static ItemHandler getItemHandler(Item item) {
+    try {
+      return ITEM_HANDLER_MAP
+        .get(ItemType.forDisplayName(item.name))
+        .getDeclaredConstructor()
+        .newInstance();
+    } catch (
+      InstantiationException
+      | IllegalAccessException
+      | InvocationTargetException
+      | NoSuchMethodException e
+    ) {
+      throw new RuntimeException(e);
+    }
+  }
+}
