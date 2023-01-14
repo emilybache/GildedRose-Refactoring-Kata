d69dbd2b17e99b986e172a5ecb27e8b19218de53
kennethongcs <kenneth.ongcs@gmail.com>
2023-01-14 16:45:48 +0800

    first commit

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,5 +1,5 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,12 +7,15 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (
+        this.items[i].name != 'Aged Brie' &&
+        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+      ) {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
@@ -21,7 +24,9 @@ class Shop {
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (
+            this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
+          ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -40,14 +45,17 @@ class Shop {
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (
+            this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+          ) {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality =
+              this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
@@ -63,5 +71,5 @@ class Shop {
 
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,57 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require('../src/gilded_rose');
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+// describe('Gilded Rose', function () {
+//   it('should foo', function () {
+//     const gildedRose = new Shop([
+//       new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0),
+//     ]);
+//     const items = gildedRose.updateQuality();
+
+//     expect(items[0].name).toBe('Backstage passes to a TAFKAL80ETC concert');
+//     expect(items[0].sellIn).toBe(-1);
+//   });
+// });
+
+// to test for Backstage passes to a TAFKAL80ETC concert
+
+// arrange
+// act
+// assert
+
+describe('Gilded Rose', function () {
+  it('Backstage pass sellIn < 11 and quality < 50', function () {
+    const sellIn = 10;
+    const quality = 40;
+
+    const gildedRose = new Shop([
+      new Item('Backstage passes to a TAFKAL80ETC concert', sellIn, quality),
+    ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+
+    expect(items[0].quality).toBe(42);
+  });
+
+  it('Backstage pass sellIn < 6 and quality < 50', function () {
+    const sellIn = 5;
+    const quality = 40;
+
+    const gildedRose = new Shop([
+      new Item('Backstage passes to a TAFKAL80ETC concert', sellIn, quality),
+    ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(43);
+  });
+
+  it('Backstage pass sellIn < 0 and quality > 50', function () {
+    const sellIn = -1;
+    const quality = 51;
+
+    const gildedRose = new Shop([
+      new Item('Backstage passes to a TAFKAL80ETC concert', sellIn, quality),
+    ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(0);
   });
 });
