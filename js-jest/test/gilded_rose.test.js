ef262b72e80f2950dca4b0598930da3d69f0b027
Facundo Alvarez <Facundosalvarez@gmail.com>
2023-02-14 11:37:05 +0100

    Update: Refactored the codebase by adding more tests and implementing conjured items.

--- js-jest/test/gilded_rose.test.js
+++
@@ -1,9 +0,0 @@
-const {Shop, Item} = require("../src/gilded_rose");
-
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
-});
