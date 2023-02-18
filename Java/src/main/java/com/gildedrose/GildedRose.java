a81faa5f29c7287639d92f3ab54e8c51ae1f96ed
pierre <pierrecrochet03@gmail.com>
2023-02-18 17:34:19 +0100

    refactor: moving up guard clause for TAFKAL80ETC Concert if quality is inferior to 50

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,16 +21,14 @@ class GildedRose {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+
                                 items[i].quality = items[i].quality + 1;
-                            }
+
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
-                            }
                         }
                     }
                 }
