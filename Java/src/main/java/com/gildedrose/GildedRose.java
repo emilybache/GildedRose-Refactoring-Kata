6154339c39e1cf6bea7eae31f4c88d6f54031738
pierre <pierrecrochet03@gmail.com>
2023-02-19 15:04:49 +0100

    refactor: simplifying the condition when the SellIn value is negative

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -34,22 +34,27 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { // continue
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        updateItem(items[i]);
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
+                if (items[i].name.equals("Aged Brie")) {
+
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
+
                 }
+                if (items[i].name.equals("+5 Dexterity Vest") | items[i].name.equals("Elixir of the Mongoose") |
+                    items[i].name.equals("Conjured Mana Cake")) {
+                    updateItem(items[i]);
+                }
+                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    items[i].quality = items[i].quality - items[i].quality;
+                }
+
+
             }
         }
     }
