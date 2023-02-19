e326caf567e70b588459fe77a5f30ec1a407f5e5
pierre <pierrecrochet03@gmail.com>
2023-02-19 15:12:49 +0100

    refactor: adding a guard clause for Sulfuras item to avoid to check later on

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,12 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+
+            // to avoid to have to check it later on
+            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                continue;
+            }
+
             // first if defnie what should decrease and what should increase
             if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") |
                 items[i].name.equals("Aged Brie")) {
@@ -25,44 +31,32 @@ class GildedRose {
                     }
                 }
             } else {
-                if (items[i].quality > 0 & !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].quality = items[i].quality - 1;
-
-                    if (items[i].name.equals("Conjured Mana Cake") & items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+                updateNormalAndConjuredItem(items[i]);
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { // continue
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            items[i].sellIn = items[i].sellIn - 1;
 
             if (items[i].sellIn < 0) {
-                if (items[i].name.equals("Aged Brie")) {
 
+                if (items[i].name.equals("Aged Brie")) {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
-
                 }
                 if (items[i].name.equals("+5 Dexterity Vest") | items[i].name.equals("Elixir of the Mongoose") |
                     items[i].name.equals("Conjured Mana Cake")) {
-                    updateItem(items[i]);
+                    updateNormalAndConjuredItem(items[i]);
                 }
                 if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     items[i].quality = items[i].quality - items[i].quality;
                 }
-
-
             }
         }
     }
 
-    public void updateItem(Item item) {
-        if (item.quality > 0 & !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+    public void updateNormalAndConjuredItem(Item item) {
+        if (item.quality > 0) {
             item.quality = item.quality - 1;
-
             if (item.name.equals("Conjured Mana Cake") && item.quality > 0) {
                 item.quality = item.quality - 1;
             }
