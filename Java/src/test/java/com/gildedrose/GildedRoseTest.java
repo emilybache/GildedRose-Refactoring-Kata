a6e85220e84eccd4b7e90744445a0897ce231d46
pierre <pierrecrochet03@gmail.com>
2023-02-19 13:48:51 +0100

    test: fine tunings the test cases to improve clarity

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -81,15 +81,13 @@ class UpdateQualityTest {
     }
 
     @Test
-    void sellInValueCanNotBeNegatifForSulfuras() {
-        System.out.println("SellIn  value of Sulfuras Item can not be negative bcse quality never decreases");
+    void sellInValueCanNotChangeForSulfuras() {
+        System.out.println("SellIn  value of Sulfuras Item can not change");
         Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -1, 80)};
         GildedRose app = new GildedRose(items);
-        int timeFrame = 10;
-        for (int i = 0; i < timeFrame; i++) {
+        for (int i = 0; i < 10; i++) {
             app.updateQuality();
         }
-        assertEquals(80, app.items[0].quality); // if time is > 1
         assertEquals(-1, app.items[0].sellIn);
     }
 
@@ -119,43 +117,37 @@ class UpdateQualityTest {
 
 
     @Test
-    void qualityItemNoMore50() {
-        System.out.println("The Quality of an item is never more than 50");
+    void qualityItemNoMore50ForAgedBrie() {
+        System.out.println("The Quality of Aged Brie item is never more than 50");
         Item[] items = new Item[]{new Item("Aged Brie", 10, 49)};
         GildedRose app = new GildedRose(items);
-        assertEquals("Aged Brie", app.items[0].name);
         app.updateQuality();
-        System.out.println("Quality: " + app.items[0].quality);
         assertEquals(50, app.items[0].quality);
         app.updateQuality();
         assertEquals(50, app.items[0].quality);
-
     }
 
     @Test
-    void qualitySulfurasItemNoMore80() {
-        System.out.println("The \"Sulfuras\"'s Quality is never more than 80");
-        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 5, 80)};
+    void qualityItemNoMore50ForBackstage() {
+        System.out.println("The Quality of Backstage passes to a TAFKAL80ETC concert item is never more than 50");
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)};
         GildedRose app = new GildedRose(items);
-        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
         app.updateQuality();
-        System.out.println("Quality: " + app.items[0].quality);
-        assertEquals(80, app.items[0].quality);
+        assertEquals(50, app.items[0].quality);
         app.updateQuality();
-        assertEquals(80, app.items[0].quality);
+        assertEquals(50, app.items[0].quality);
     }
-
 
     @Test
     void itemSulfurasNotChangeQuality() {
         System.out.println("\"Sulfuras\", being a legendary item, never decreases in Quality and stays the same");
-        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 5, 44)};
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 5, 55)};
         GildedRose app = new GildedRose(items);
         assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
         for (int i = 0; i < 10; i++) {
             app.updateQuality();
         }
-        assertEquals(44, app.items[0].quality);
+        assertEquals(55, app.items[0].quality);
     }
 
     @Test
@@ -164,13 +156,14 @@ class UpdateQualityTest {
         Item[] items = new Item[]{
             new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0),
             new Item("Backstage passes to a TAFKAL80ETC concert", 9, 0),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 8, 0)
+            new Item("Backstage passes to a TAFKAL80ETC concert", 8, 49)
         };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        for (int i = 0; i < 2; i++) {
-            assertEquals(2, app.items[i].quality);
-        }
+        assertEquals(2, app.items[0].quality);
+        assertEquals(2, app.items[1].quality);
+        assertEquals(50, app.items[2].quality);
+
     }
 
     @Test
@@ -179,13 +172,13 @@ class UpdateQualityTest {
         Item[] items = new Item[]{
             new Item("Backstage passes to a TAFKAL80ETC concert", 5, 3),
             new Item("Backstage passes to a TAFKAL80ETC concert", 4, 3),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 2, 3)
+            new Item("Backstage passes to a TAFKAL80ETC concert", 2, 49)
         };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        for (int i = 0; i < 2; i++) {
-            assertEquals(6, app.items[i].quality);
-        }
+        assertEquals(6, app.items[0].quality);
+        assertEquals(6, app.items[1].quality);
+        assertEquals(50, app.items[2].quality);
 
     }
 
@@ -194,14 +187,15 @@ class UpdateQualityTest {
         System.out.println("Quality drops to 0 after the concert");
         Item[] items = new Item[]{
             new Item("Backstage passes to a TAFKAL80ETC concert", 0, 2),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 3),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 7)
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 11),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 48)
         };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        for (Item el : items) {
-            assertEquals(0, el.quality);
-        }
+        assertEquals(0, app.items[0].quality);
+        assertEquals(0, app.items[1].quality);
+        assertEquals(0, app.items[2].quality);
+
     }
 
     @Test
@@ -212,9 +206,7 @@ class UpdateQualityTest {
         };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        for (Item el : items) {
-            assertEquals(0, el.quality);
-        }
+            assertEquals(0, app.items[0].quality);
     }
 
     /**
