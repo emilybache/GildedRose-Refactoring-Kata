15f4d079a6e8c4db0e497c6cd3e69714b44a31e6
pierre <pierrecrochet03@gmail.com>
2023-02-19 14:07:23 +0100

    refactor: adding a guard clause in the updateItem method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,19 +11,17 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                 && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    updateItem(items[i]);
-                }
+                updateItem(items[i]);
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && (items[i].quality < 50)) {
-                        if (items[i].sellIn < 11) {
-                                items[i].quality = items[i].quality + 1;
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
-                        if (items[i].sellIn < 6) {
-                                items[i].quality = items[i].quality + 1;
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
                     }
                 }
@@ -36,9 +34,7 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            updateItem(items[i]);
-                        }
+                        updateItem(items[i]);
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
@@ -50,16 +46,16 @@ class GildedRose {
             }
         }
     }
-     public void updateItem(Item item){
-         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-             item.quality = item.quality - 1;
 
-             if (item.name.equals("Conjured Mana Cake") && item.quality > 0) {
-                 item.quality = item.quality - 1;
-             }
-         }
+    public void updateItem(Item item) {
+            if (item.quality > 0 & !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.quality = item.quality - 1;
 
+                if (item.name.equals("Conjured Mana Cake") && item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+        }
 
-     }
+    }
 
 }
