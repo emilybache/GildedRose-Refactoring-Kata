8a2e2e787651d9085e97823d39d2da0ea5d376c3
pierre <pierrecrochet03@gmail.com>
2023-02-19 16:16:51 +0100

    test: adding some others units tests for object Item

--- Java/src/test/java/com/gildedrose/ItemTest.java
+++ Java/src/test/java/com/gildedrose/ItemTest.java
@@ -4,12 +4,51 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class  ItemTest{
+class  ItemTest {
 
     @Test
-    void toImplement() {
-        //todo testing the class
-        assertEquals(true,true);
+    void contructorWithThreeArguments() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 5, 7)};
+        assertEquals("Elixir of the Mongoose", items[0].getName());
+        assertEquals(7, items[0].getQuality());
+        assertEquals(5, items[0].getSellIn());
     }
 
+    @Test
+    void getterName() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 0, 0)};
+        assertEquals("Elixir of the Mongoose", items[0].getName());
     }
+
+    @Test
+    void getterSellin() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 7, 0)};
+        assertEquals(7, items[0].getSellIn());
+        assertEquals(0, items[0].getQuality());
+
+    }
+
+    @Test
+    void setterSellin() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 7, 0)};
+        items[0].setSellIn(1);
+        assertEquals(1, items[0].getSellIn());
+        assertEquals(0, items[0].getQuality());
+    }
+
+
+    @Test
+    void getterQuality() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 0, 5)};
+        assertEquals(5, items[0].getQuality());
+        assertEquals(0, items[0].getSellIn());
+    }
+
+    @Test
+    void setterQuality() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 0, 5)};
+        items[0].setQuality(1);
+        assertEquals(1, items[0].getQuality());
+        assertEquals(0, items[0].getSellIn());
+    }
+}
