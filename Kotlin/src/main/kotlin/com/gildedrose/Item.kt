31b91e2980e0ff87960517da2c072baa2d378c8a
Thang Tran <thang.tran@nfq.asia>
2023-03-01 13:06:58 +0700

    Remove some redundant files

--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++
@@ -1,7 +0,0 @@
-package com.gildedrose
-
-open class Item(var name: String, var sellIn: Int, var quality: Int) {
-    override fun toString(): String {
-        return this.name + ", " + this.sellIn + ", " + this.quality
-    }
-}
