0fda48419e63a6250690dfeaa8972fac36e546d3
Lekshmy <lekshmysasidhar@gmail.com>
2023-03-31 09:21:21 +0200

    Code Refactoring : Extracted logic of sellin days to a new method.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	void testUpdateSellInDaysForAgedBrie() {
+		Item[] items = new Item[] { new Item("Aged Brie", 1, 1) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(0, items[0].sellIn);
+	}
+
+	@Test
+	void testUpdateSellInDaysForSulfuras() {
+		Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 1) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(1, items[0].sellIn);
+	}
 
 }
