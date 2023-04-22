17d88159f8a93bc5f85aa244feb462961b450f04
Chaplin Artem <75485190+ArCheeq@users.noreply.github.com>
2023-04-22 18:32:48 +0300

    fix: fix test function

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -5,7 +5,7 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+    expect(items[0].name).to.equal("foo");
   });
 
 });
