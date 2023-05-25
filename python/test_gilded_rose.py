dedad50044168352784868a28156e6d9076b84dc
Kseniia Kutsenko <kseniia.kutsenko.kh@gmail.com>
2023-05-25 14:13:40 +0300

    Update test_gilded_rose.py

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import unittest
 
 from gilded_rose import Item, GildedRose
@@ -9,8 +8,8 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
