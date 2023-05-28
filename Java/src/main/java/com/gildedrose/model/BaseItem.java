ce49c32e5e0b935d4ab284d8f1812b306e3cfda2
Ibrahim Dogrusoz <idogrusoz@gmail.com>
2023-05-28 16:05:52 +0200

    Encapsulate Item class

---
+++ Java/src/main/java/com/gildedrose/model/BaseItem.java
@@ -0,0 +1,10 @@
+package com.gildedrose.model;
+
+import com.gildedrose.Item;
+
+public class BaseItem {
+    private final Item item;
+    public BaseItem(Item item) {
+        this.item = item;
+    }
+}
