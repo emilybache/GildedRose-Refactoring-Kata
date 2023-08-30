ad56fa1ce09f8830f6fc6e8dd9ec7945bebbd1f9
Maxim Stomphorst <maxim.stomphorst@han.nl>
2023-08-30 20:54:41 +0200

    cleanup

--- csharpcore/GildedRoseTests/ApprovalTest.cs
+++ csharpcore/GildedRoseTests/ApprovalTest.cs
@@ -3,27 +3,24 @@ using System.IO;
 using System.Text;
 using ApprovalTests;
 using ApprovalTests.Reporters;
-using GildedRoseTests;
 using NUnit.Framework;
 
-namespace csharp
+namespace GildedRoseTests;
+
+[UseReporter(typeof(DiffReporter))]
+[TestFixture]
+public class ApprovalTest
 {
-    [UseReporter(typeof(DiffReporter))]
-    [TestFixture]
-    public class ApprovalTest
+    [Test]
+    public void ThirtyDays()
     {
-        [Test]
-        public void ThirtyDays()
-        {
-            
-            StringBuilder fakeoutput = new StringBuilder();
-            Console.SetOut(new StringWriter(fakeoutput));
-            Console.SetIn(new StringReader("a\n"));
+        StringBuilder fakeOutput = new StringBuilder();
+        Console.SetOut(new StringWriter(fakeOutput));
+        Console.SetIn(new StringReader($"a{Environment.NewLine}"));
 
-            TexttestFixture.Main(new string[] { });
-            var output = fakeoutput.ToString();
+        TextTestFixture.Main(new string[] { });
+        var output = fakeOutput.ToString();
 
-            Approvals.Verify(output);
-        }
+        Approvals.Verify(output);
     }
 }
