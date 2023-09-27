efcaa3b33dd6cf87dae3d899363f013070f54324
Ashwin Rana <119340735+gaiters-cafes@users.noreply.github.com>
2023-09-27 16:35:28 +0000

    sulfuras unittest

--- python/tests/test_sulfuras.py
+++ python/tests/test_sulfuras.py
@@ -4,13 +4,14 @@ SULFURAS = "Sulfuras, Hand of Ragnaros"
 
 # "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
 def test_sulfuras_sell_in_does_not_decrease():
-    items = [Item(SULFURAS, 0, 0)]
+    items = [Item(SULFURAS, 0, 80)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     assert items[0].sell_in == 0
 
 def test_sulfuras_does_not_decrease_in_quality():
-    items = [Item(SULFURAS, 0, 0)]
+    items = [Item(SULFURAS, 0, 80)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
-    assert items[0].quality == 0
+    assert items[0].quality == 80
+
