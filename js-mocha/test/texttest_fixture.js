7b973f93ddbd4c9a4603d8416d50a584326fa47c
emily <emily@bacheconsulting.com>
2023-11-03 15:39:50 +0100

    Make javascript mocha texttests work

--- js-mocha/test/texttest_fixture.js
+++ js-mocha/test/texttest_fixture.js
@@ -19,9 +19,10 @@ const days = Number(process.argv[2]) || 2;
 const gildedRose = new Shop(items);
 
 console.log("OMGHAI!");
-for (let day = 0; day < days; day++) {
-  console.log(`\n-------- day ${day} --------`);
+for (let day = 0; day < days + 1; day++) {
+  console.log(`-------- day ${day} --------`);
   console.log("name, sellIn, quality");
   items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
   gildedRose.updateQuality();
+  console.log("")
 }
