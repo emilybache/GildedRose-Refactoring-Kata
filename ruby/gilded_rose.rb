bcf8139af4451db9fa2b620558285cb3db6de4a7
Yash Dubey <yash.dubey@protonshub.com>
2023-11-29 22:37:26 +0530

    Fixed test cases

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -11,13 +11,14 @@ class GildedRose
       # Decreasing the quality of item
       if !item.is_aged_brie? && !item.is_backstage_passes? && !item.is_sulfuras? && item.quality > 0
         if item.sell_in < 0
-          item.quality = item.quality - 4 if item.is_conjured_mana_cake?
-          item.quality = item.quality - 2 if !item.is_conjured_mana_cake?
+          item.quality = item.quality - 4 if item.is_conjured_mana_cake? # Decreasing twice quality if item is conjured_mana_cake
+          item.quality = item.quality - 2 if !item.is_conjured_mana_cake? # Decreasing 2 quality if item is not conjured_mana_cake
         else
-          item.quality = item.quality - 2 if item.is_conjured_mana_cake?
-          item.quality = item.quality - 1 if !item.is_conjured_mana_cake?
+          item.quality = item.quality - 2 if item.is_conjured_mana_cake? # Decreasing twice quality if item is conjured_mana_cake
+          item.quality = item.quality - 1 if !item.is_conjured_mana_cake? # Decreasing 2 quality if item is not conjured_mana_cake
         end
       elsif item.quality < 50 && (item.is_backstage_passes? || item.is_aged_brie?)
+        # Increasing quality for backstage passes and aged brie item according based on sell in days
         if item.sell_in < 11
           item.quality = item.quality + 2
         elsif item.sell_in < 6
@@ -25,16 +26,19 @@ class GildedRose
         else 
           item.quality = item.quality + 1
         end
-        item.quality = 50 if item.quality > 50
+        item.quality = 50 if item.quality > 50 # Insuring item quality should not get over 50
+        item.quality = 0 if item.quality < 0 # Insuring item quality should not be negative
       end
 
       if !item.is_sulfuras?
-        item.sell_in = item.sell_in - 1
+        item.sell_in = item.sell_in - 1 # Decreasing sell in daily if item is not sulfuras
       end
 
       if item.sell_in < 0
+        # Setting quality to 0 if concert is over
         item.quality = item.quality - item.quality if item.is_aged_brie? || item.is_backstage_passes?
       end
+      item.quality = 50 if item.quality > 50 && item.is_sulfuras?
     end
   end
 end
