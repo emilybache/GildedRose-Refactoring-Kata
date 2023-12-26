7658124173cfa1236200aac26334e60943cc5fbe
Nitsan Avni <nitsanav@gmail.com>
2023-12-26 12:27:11 +0100

    gilded_rose.sh: closer to original

--- bash/gilded_rose.sh
+++ bash/gilded_rose.sh
@@ -10,42 +10,46 @@ update_quality() {
         if [[ $name != "Aged Brie" && $name != "Backstage passes to a TAFKAL80ETC concert" ]]; then
             if ((quality > 0)); then
                 if [[ $name != "Sulfuras, Hand of Ragnaros" ]]; then
-                    ((quality--))
+                    quality=$((quality - 1))
                 fi
             fi
         else
             if ((quality < 50)); then
-                ((quality++))
+                quality=$((quality + 1))
 
                 if [[ $name == "Backstage passes to a TAFKAL80ETC concert" ]]; then
-                    if ((sell_in < 11)) && ((quality < 50)); then
-                        ((quality++))
+                    if ((sell_in < 11)); then
+                        if ((quality < 50)); then
+                            quality=$((quality + 1))
+                        fi
                     fi
-                    if ((sell_in < 6)) && ((quality < 50)); then
-                        ((quality++))
+                    if ((sell_in < 6)); then
+                        if ((quality < 50)); then
+                            quality=$((quality + 1))
+                        fi
                     fi
                 fi
             fi
         fi
 
         if [[ $name != "Sulfuras, Hand of Ragnaros" ]]; then
-            ((sell_in--))
+            sell_in=$((sell_in - 1))
+        fi
 
-            if ((sell_in < 0)); then
-                if [[ $name != "Aged Brie" ]]; then
-                    if [[ $name != "Backstage passes to a TAFKAL80ETC concert" ]]; then
-                        if ((quality > 0)); then
-                            if [[ $name != "Sulfuras, Hand of Ragnaros" ]]; then
-                                ((quality--))
-                            fi
+        if ((sell_in < 0)); then
+            if [[ $name != "Aged Brie" ]]; then
+                if [[ $name != "Backstage passes to a TAFKAL80ETC concert" ]]; then
+                    if ((quality > 0)); then
+                        if [[ $name != "Sulfuras, Hand of Ragnaros" ]]; then
+                            quality=$((quality - 1))
                         fi
-                    else
-                        quality=0
                     fi
                 else
-                    if ((quality < 50)); then
-                        ((quality++))
-                    fi
+                    quality=$((quality - quality))
+                fi
+            else
+                if ((quality < 50)); then
+                    quality=$((quality + 1))
                 fi
             fi
         fi
