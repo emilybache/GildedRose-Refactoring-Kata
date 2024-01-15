6109899ada2ca31e3ea3bf9b980d2365fe8ab8b9
Chamoda Ranasinghe <chamodam@gmail.com>
2024-01-16 03:53:34 +0700

    added CONJURED_CAKE key to ItemNames constant

--- TypeScript/app/constants.ts
+++ TypeScript/app/constants.ts
@@ -5,6 +5,7 @@ const ItemNames = {
   AGED_BRIE: "Aged Brie",
   BACKSTAGE_PASSES: "Backstage passes to a TAFKAL80ETC concert",
   SULFURAS: "Sulfuras, Hand of Ragnaros",
+  CONJURED_CAKE: "Conjured Mana Cake",
 };
 
 const MAX_ITEM_QUALITY = 50;
