ea9b680b554be2e70ce7b8cb758fca4a6808f477
Sam Burns <sam.burns@upmind.com>
2024-01-24 15:54:07 +0000

    To simplify code, remove old business logic

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -32,25 +32,13 @@ final class GildedRose
                 continue;
             }
 
-            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->name !== 'Aged Brie') {
                 if ($item->quality > 0) {
                     $item->quality = $item->quality - 1;
                 }
             } else {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
                 }
             }
 
@@ -58,12 +46,8 @@ final class GildedRose
 
             if ($item->sellIn < 0) {
                 if ($item->name !== 'Aged Brie') {
-                    if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            $item->quality = $item->quality - 1;
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
+                    if ($item->quality > 0) {
+                        $item->quality = $item->quality - 1;
                     }
                 } else {
                     if ($item->quality < 50) {
