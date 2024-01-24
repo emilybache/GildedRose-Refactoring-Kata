dcde3a22a2218dc981786ca212a463dbfc156123
Sam Burns <sam.burns@upmind.com>
2024-01-24 16:02:03 +0000

    To simplify code, remove old business logic

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -32,27 +32,15 @@ final class GildedRose
                 continue;
             }
 
-            if ($item->name !== 'Aged Brie') {
-                if ($item->quality > 0) {
-                    $item->quality = $item->quality - 1;
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
             }
 
             $item->sellIn = $item->sellIn - 1;
 
             if ($item->sellIn < 0) {
-                if ($item->name !== 'Aged Brie') {
-                    if ($item->quality > 0) {
-                        $item->quality = $item->quality - 1;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+                if ($item->quality > 0) {
+                    $item->quality = $item->quality - 1;
                 }
             }
         }
