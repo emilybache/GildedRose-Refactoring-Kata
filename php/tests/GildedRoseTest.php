fb7d21042eeb8018cc00eb60eb4887e9d3dd4768
Sam Burns <sam.burns@upmind.com>
2024-01-24 13:00:14 +0000

    To provide a basic test harness using existing infrastructure, add approval test expectations

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -15,6 +15,6 @@ class GildedRoseTest extends TestCase
         $items = [new Item('foo', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame('foo', $items[0]->name);
     }
 }
