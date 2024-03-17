2a1d702f79fda37ec5599d3c4c5d54b5cc0bf009
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2024-03-17 22:29:09 +0100

    Testing decreasing quality of multiple items

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,6 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
+    void should_decrease_multiple_items_quality_each_day() {
+        Item firstItem = new Item("First Standard Item", 5, 4);
+        Item secondItem = new Item("Second Standard Item", 3, 2);
+        GildedRose gildedRose = new GildedRose(new Item[] { firstItem, secondItem });
+
+        gildedRose.updateQuality();
+
+        assertEquals(firstItem.sellIn, 4);
+        assertEquals(firstItem.quality, 3);
+        assertEquals(secondItem.sellIn, 2);
+        assertEquals(secondItem.quality, 1);
+    }
+
+    @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
@@ -15,6 +29,7 @@ class GildedRoseTest {
     }
 
     @Test
+
     void should_decrease_sellin_and_quality_values_each_day() {
         int sellinStart = 6;
         int qualityStart = 8;
