63977832589c8b7ad5bde1ac9c520dd7a098923d
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2024-03-17 22:23:08 +0100

    Test decreasin quality and sellin values on standard items each day

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,16 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void should_decrease_sellin_and_quality_values_each_day() {
+        int sellinStart = 6;
+        int qualityStart = 8;
+        final Item standardItem = new Item("Some item", sellinStart, qualityStart);
+        GildedRose gildedRose = new GildedRose(new Item[] {standardItem});
+
+        gildedRose.updateQuality();
+
+        assertEquals(standardItem.sellIn,sellinStart - 1);
+        assertEquals(standardItem.quality, qualityStart - 1);
+    }
 }
