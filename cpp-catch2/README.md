661b131832520518f55b2e3ecfd2bfedae611ede
emily <emily@bacheconsulting.com>
2024-04-02 16:54:07 +0200

    add more explicit approval test to cpp catch2 version

--- cpp-catch2/README.md
+++ cpp-catch2/README.md
@@ -1,10 +1,11 @@
 C++ version of Gilded Rose with Catch 2 and Approvals
 ======================================================
 
-This is a C++ start of the ApprovalTest version of the Gilded Rose Refactoring Kata. See
+This is a C++ start of the Gilded Rose Refactoring Kata. See
 the [top level readme](https://github.com/emilybache/GildedRose-Refactoring-Kata)
  for a general description of the exercise.
 
+There are two (failing) unit tests included here. One uses only the catch2 framework, the other additionally uses [ApprovalTests](https://github.com/approvals/approvaltests.cpp). You should choose one of these tests to work with and delete the other.
 
 CMake
 -----
--- cpp-catch2/test/gildedrose_catch.cpp
+++ cpp-catch2/test/gildedrose_catch.cpp
@@ -11,6 +11,7 @@ std::ostream& operator<<(std::ostream& os, const Item& obj)
             << ", quality: " << obj.quality;
 }
 
+// This is a normal unit test using Catch2
 TEST_CASE("UpdateQuality") {
 
     vector<Item> items;
@@ -18,4 +19,14 @@ TEST_CASE("UpdateQuality") {
     GildedRose app(items);
     app.updateQuality();
     REQUIRE("fixme" == app.items[0].name);
+}
+
+// This is an Approval test using https://github.com/approvals/approvaltests.cpp
+TEST_CASE("UpdateQualityApprovalTest") {
+    vector<Item> items;
+    items.push_back(Item("foo", 0, 0));
+    GildedRose app(items);
+    app.updateQuality();
+    auto item = app.items[0];
+    ApprovalTests::Approvals::verify(item);
 }
