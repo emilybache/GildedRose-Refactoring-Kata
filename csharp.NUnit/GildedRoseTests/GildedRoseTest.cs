6e5067dcb9c366e7ab09443b748a009536506ef4
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-05-29 15:01:51 +0200

    Refactor C# projects

---
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Test]
+    public void Foo()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+    }
+}
