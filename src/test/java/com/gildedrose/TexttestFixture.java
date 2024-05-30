3d310499ac6dd1cee86a06a6a223b1b5004b2a74
Chase Barrett <chase.barrett@delinea.com>
2024-05-30 13:47:10 -0600

    Refactor to focus on Jupiter based testing.

---
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -0,0 +1,46 @@
+package com.gildedrose;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class TexttestFixture {
+    public static void main(String[] args) {
+
+        try (PrintStream out = new PrintStream("src/test/TexttestFixture.received.txt")) {
+            out.println("OMGHAI!");
+
+            Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+
+            GildedRose app = new GildedRose(items);
+
+            int days = 2;
+            if (args.length > 0) {
+                days = Integer.parseInt(args[0]) + 1;
+            }
+
+            for (int i = 0; i < days; i++) {
+                out.println("-------- day " + i + " --------");
+                out.println("name, sellIn, quality");
+                for (Item item : items) {
+                    out.println(item);
+                }
+                out.println();
+                app.updateQuality();
+            }
+        } catch (final IOException e) {
+            System.err.println("Problem! " + e.getMessage());
+        }
+    }
+
+}
---
+++ src/test/resources/TexttestFixture.accepted.txt
@@ -0,0 +1,24 @@
+OMGHAI!
+-------- day 0 --------
+name, sellIn, quality
++5 Dexterity Vest, 10, 20
+Aged Brie, 2, 0
+Elixir of the Mongoose, 5, 7
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 15, 20
+Backstage passes to a TAFKAL80ETC concert, 10, 49
+Backstage passes to a TAFKAL80ETC concert, 5, 49
+Conjured Mana Cake, 3, 6
+
+-------- day 1 --------
+name, sellIn, quality
++5 Dexterity Vest, 9, 19
+Aged Brie, 1, 1
+Elixir of the Mongoose, 4, 6
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 14, 21
+Backstage passes to a TAFKAL80ETC concert, 9, 50
+Backstage passes to a TAFKAL80ETC concert, 4, 50
+Conjured Mana Cake, 2, 5
