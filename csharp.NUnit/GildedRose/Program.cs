995c40fbd8febf65c66688970e4cc67241cb5eb8
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-06-02 11:14:17 +0200

    days

--- csharp.NUnit/GildedRose/Program.cs
+++ csharp.NUnit/GildedRose/Program.cs
@@ -41,8 +41,13 @@ public class Program
 
         var app = new GildedRose(items);
 
+        int days = 2;
+        if (args.Length > 0)
+        {
+            days = int.Parse(args[0]) + 1;
+        }
 
-        for (var i = 0; i < 31; i++)
+        for (var i = 0; i < days; i++)
         {
             Console.WriteLine("-------- day " + i + " --------");
             Console.WriteLine("name, sellIn, quality");
