67d0ff72347923657a288fa43c1e4ae5d2153b33
Felipe Gaete <fgaete@buk.cl>
2024-07-28 21:59:17 -0400

    test: agrega tests no implementados para rspec ruby

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -8,4 +8,40 @@ describe GildedRose do
     GildedRose.new(items).update_quality()
     expect(items[0].name).to eq "fixme"
   end
+
+  it 'decrementa la calidad del item'
+
+  it 'decrementa los dias para vender el item'
+
+  it 'decrementa la calidad del item al doble de velocidad cuando ya no quedan dias para venderlo'
+
+  it 'no decrementa la calidad del item a negativo'
+
+  context 'para items Aged Brie' do
+    it 'incrementa su calidad'
+
+    it 'incrementa su calidad en 2 cuando ya no quedan dias de venta'
+
+    it 'no incrementa su calidad por sobre 50'
+  end
+
+  context 'para items Sulfuras' do
+    it 'no cambia su calidad'
+
+    it 'no cambia los dias para venderlo'
+  end
+
+  context 'para items Backstage Pass' do
+    it 'incrementa su calidad si quedan más de 10 días para venderlo'
+
+    it 'incrementa su calidad en 2 si quedan 10 dias o menos para venderlo'
+
+    it 'incrementa su calidad en 3 si quedan 5 dias o menos para venderlo'
+
+    it 'decrementa su calidad a 0 si ya no quedan dias para venderlo'
+  end
+
+  context 'para items conjurados' do
+    it 'decrementa su calidad en 2'
+  end
 end
