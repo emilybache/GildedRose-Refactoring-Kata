a8c22775923eb19195c203057a3bebc2a5fb5b10
Agustin Bereciartua <abereciartua@buk.cl>
2024-08-05 10:51:56 -0400

    tests: add tests for `Aged Brie incrementa la calida` test case

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,19 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+describe("GildedRose", () => {
+  describe("updateQuality", () => {
+    describe("Aged Brie", () => {
+      it("should increase quality when updateQuality is called", () => {
+        // Test code here
+      });
+
+      it("should not increase quality above 50", () => {
+        // Test code here
+      });
+
+      it("should increase quality by 2 after sellIn date has passed", () => {
+        // Test code here
+      });
+    });
   });
 });
