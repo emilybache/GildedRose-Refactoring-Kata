3aefb4ae80a30ac7a90afcc954cef562c17b4cc8
Agustin Bereciartua <abereciartua@buk.cl>
2024-08-05 10:51:56 -0400

    tests: add tests for `Aged Brie incrementa la calida` test case

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,34 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+describe("GildedRose", () => {
+  describe("updateQuality", () => {
+    describe("Aged Brie", () => {
+      it("should increase quality when updateQuality is called", () => {
+        const items = [new Item("Aged Brie", 10, 10)];
+        const gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        expect(gildedRose.items[0].quality).toBe(11);
+      });
+
+      it("should not increase quality above 50", () => {
+        const items = [new Item("Aged Brie", 10, 50)];
+        const gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        expect(gildedRose.items[0].quality).toBe(50);
+      });
+
+      it("should increase quality by 2 after sellIn date has passed", () => {
+        const items = [new Item("Aged Brie", 0, 10)];
+        const gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        expect(gildedRose.items[0].quality).toBe(12);
+      });
+    });
   });
 });
