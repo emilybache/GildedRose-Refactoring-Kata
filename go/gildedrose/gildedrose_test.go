46e41c6d5ab4d38add357c0c2c2174defc2c73c1
david.wolfson <david.wolfson@bjss.com>
2024-10-07 14:06:25 +0100

    Test_foo now testing name, SellIn and Quality

--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -21,6 +21,12 @@ func Test_Foo(t *testing.T) {
 
 	// Assert
 	if items[0].Name != expected[0].Name {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].Name)
+		t.Errorf("Name: Expected %s but got %s ", expected[0].Name, items[0].Name)
+	}
+	if items[0].SellIn != expected[0].SellIn {
+		t.Errorf("SellIn: Expected %d but got %d ", expected[0].SellIn, items[0].SellIn)
+	}
+	if items[0].Quality != expected[0].Quality {
+		t.Errorf("Quality: Expected %d but got %d ", expected[0].Quality, items[0].Quality)
 	}
 }
