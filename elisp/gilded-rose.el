8fb5a6a35bdbf81215e1914803bef2338e0a39e9
mgd <mgd@posteo.net>
2024-10-09 00:50:47 +0100

    Further Amendments
    - removed the cond statement and replaced with nested ifs
    - removed the let statement. Data is now accesses directly
    - update-quality now takes a list of items

--- elisp/gilded-rose.el
+++ elisp/gilded-rose.el
@@ -2,22 +2,26 @@
   "Create an item with NAME, SELL-IN, and QUALITY."
   (list :name name :sell-in sell-in :quality quality))
 
-(defun update-quality (item)
-  (let* ((quality (plist-get item :quality))
-         (sell-in (plist-get item :sell-in))
-         (name (plist-get item :name)))
-    (cond
-     ((string= name "Aged Brie")
-      (setf (nth 2 item) (min 50 (1+ quality))))
-     ((string= name "Backstage passes")
-      (cond
-       ((> sell-in 10) (setf (nth 2 item) quality))
-       ((and (<= sell-in 10) (> sell-in 5)) (setf (nth 2 item) (min 50 (+ quality 2))))
-       ((and (<= sell-in 5) (> sell-in 0)) (setf (nth 2 item) (min 50 (+ quality 3))))
-       (t (setf (nth 2 item) 0))))
-     ((string= name "Sulfuras")
-      (setf (nth 2 item) quality))
-     (t
-      (setf (nth 2 item) (max 0 (1- quality)))))))
+(defun update-quality (items)
+  (dolist (item items)
+    (if (string= (plist-get item :name) "Aged Brie")
+        (plist-put item :quality (min 50 (1+ (plist-get item :quality))))
+      
+      (if (string= (plist-get item :name) "Backstage passes")
+          (if (> (plist-get item :sell-in) 10)
+              (plist-put item :quality (plist-get item :quality))
+            
+            (if (and (<= (plist-get item :sell-in) 10) (> (plist-get item :sell-in) 5))
+                (plist-put item :quality (min 50 (+ (plist-get item :quality) 2)))
+              
+              (if (and (<= (plist-get item :sell-in) 5) (> (plist-get item :sell-in) 0))
+                  (plist-put item :quality (min 50 (+ (plist-get item :quality) 3)))
+                
+                (plist-put item :quality 0))))
+        
+        (if (string= (plist-get item :name) "Sulfuras")
+            (plist-put item :quality (plist-get item :quality))
+          
+          (plist-put item :quality (max 0 (1- (plist-get item :quality)))))))))
 
 (provide 'gilded-rose)
