68b45d2da9a61f4fe6ed24a63c5ed0bda3f02bd3
Ahmed Abdeen <abdeenahmed2002@hotmail.com>
2025-01-16 01:19:50 +0100

    feat: conjured-items - Updated the result file to include the "conjured" items.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 class GildedRoseTest {
     @Test
     void test() {
-        Item[] items = new Item[] {new Item("foo", 0, 0)};
+        Item[] items = new Item[] {new Item(GildedRose.CONJURED, -1, 4)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.getItems()[0].name);
+        assertEquals(GildedRose.CONJURED, app.getItems()[0].name);
+        assertEquals(0, app.getItems()[0].quality);
+        assertEquals(-2, app.getItems()[0].sellIn);
     }
 }
