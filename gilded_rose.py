984f5e61daabd5b3935ed1967fc77cb4c6f9f0fc
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 15:08:22 +1030

    r lift up condition

--- gilded_rose.py
+++ gilded_rose.py
@@ -21,29 +21,54 @@ def update_item(item: Item):
 
     else:
         is_backstage_pass = item.name == "Backstage passes to a TAFKAL80ETC concert"
-        
-        if not False and not is_backstage_pass:
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if not is_backstage_pass:
+
+        if is_backstage_pass:
+            if not False and not True:
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
             else:
-                item.quality = item.quality - item.quality
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                if not True:
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
+
+        else:
+            if not False and not False:
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                if not False:
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
 
 
 class GildedRose:
