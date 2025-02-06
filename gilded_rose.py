13dde323cbc629dcca7506074a6e668ad6f41de6
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 16:05:09 +1030

    r rename func

--- gilded_rose.py
+++ gilded_rose.py
@@ -9,22 +9,11 @@ class Item:
 
 
 def update_item(item: Item):
-    if is_sulfuras(item):
+    if should_do_nothing(item):
         return
 
     if is_backstage_pass(item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-            if item.sell_in < 11:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-            if item.sell_in < 6:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            item.quality = item.quality - item.quality
-        return
+        return update_backstage_pass(item)
 
     if is_aged_brie(item):
         if item.quality < 50:
@@ -56,9 +45,24 @@ def is_aged_brie(item: Item):
     return item.name == "Aged Brie"
 
 
-def is_sulfuras(item: Item):
+def should_do_nothing(item: Item):
     return item.name == "Sulfuras, Hand of Ragnaros"
 
 
 def is_backstage_pass(item: Item):
     return item.name == "Backstage passes to a TAFKAL80ETC concert"
+
+
+def update_backstage_pass(item: Item):
+    if item.quality < 50:
+        item.quality = item.quality + 1
+        if item.sell_in < 11:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+        if item.sell_in < 6:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0:
+        item.quality = item.quality - item.quality
+    return
