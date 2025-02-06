3c4653f45dfcd882e440c63950808e51d6272813
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 15:13:57 +1030

    r guard clause

--- gilded_rose.py
+++ gilded_rose.py
@@ -41,13 +41,12 @@ def update_item(item: Item):
             if is_sulfuras:
                 return
 
-            else:
+            if item.quality > 0:
+                item.quality = item.quality - 1
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
                 if item.quality > 0:
                     item.quality = item.quality - 1
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
 
 
 class GildedRose:
