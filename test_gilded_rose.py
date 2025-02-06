634a3a89b5523f713ada224f726189973013fb9a
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 14:15:03 +1030

    t approval actual name

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -9,8 +9,8 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        verify("fixme")
+        verify(items[0].name)
 
-        
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
