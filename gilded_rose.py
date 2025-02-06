fe50ef0945ff2302a2eabf76ee944ec4858f2966
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 15:08:58 +1030

    r rm dead code

--- gilded_rose.py
+++ gilded_rose.py
@@ -23,43 +23,25 @@ def update_item(item: Item):
         is_backstage_pass = item.name == "Backstage passes to a TAFKAL80ETC concert"
 
         if is_backstage_pass:
-            if not False and not True:
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.sell_in < 11:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if not True:
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
+                item.quality = item.quality - item.quality
 
         else:
             if not False and not False:
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -67,8 +49,6 @@ def update_item(item: Item):
                     if item.quality > 0:
                         if item.name != "Sulfuras, Hand of Ragnaros":
                             item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
 
 
 class GildedRose:
