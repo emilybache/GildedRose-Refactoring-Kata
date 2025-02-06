c7863fc4de7be3470bd69e4e4305131cb3a3b04b
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 16:38:04 +1030

    t finish feature

--- gilded_rose.py
+++ gilded_rose.py
@@ -28,11 +28,11 @@ def update_item(item: Item):
         return
 
     if item.quality > 0:
-        item.quality = item.quality - 1
+        item.quality = item.quality - (1 if item.name != "Conjured" else 2)
     item.sell_in = item.sell_in - 1
     if item.sell_in < 0:
         if item.quality > 0:
-            item.quality = item.quality - 1
+            item.quality = item.quality - (1 if item.name != "Conjured" else 2)
 
 
 class GildedRose:
