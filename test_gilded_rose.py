a810814a323b268e855b0821b411045c0a0ef5e3
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 14:23:27 +1030

    r loop thru each indiv

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -12,13 +12,21 @@ class GildedRoseTest(unittest.TestCase):
             Item("Aged Brie", 0, 0),
             Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),
         ]
+        input_Vals = [
+            ("foo", 0, 0),
+            ("Aged Brie", 0, 0),
+            ("Backstage passes to a TAFKAL80ETC concert", 0, 0),
+        ]
         output_items = deepcopy(inputs)
         gilded_rose = GildedRose(output_items)
         gilded_rose.update_quality()
 
         to_approve = []
-        for item in output_items:
-            to_approve.append(str(item))
+        for item in input_Vals:
+            thing = Item(item[0], item[1], item[2])
+            gilded_rose = GildedRose([thing])
+            gilded_rose.update_quality()
+            to_approve.append(str(thing))
 
         verify("\n".join(to_approve))
 
