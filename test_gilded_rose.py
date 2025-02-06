c7e24b66cd0cdc338ab3224f7b0253c140d2ef0f
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 14:22:08 +1030

    r immutable

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import unittest
 
 from gilded_rose import Item, GildedRose
@@ -6,16 +7,17 @@ from approvaltests.approvals import verify
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [
+        inputs = [
             Item("foo", 0, 0),
             Item("Aged Brie", 0, 0),
             Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),
         ]
-        gilded_rose = GildedRose(items)
+        output_items = deepcopy(inputs)
+        gilded_rose = GildedRose(output_items)
         gilded_rose.update_quality()
 
         to_approve = []
-        for item in items:
+        for item in output_items:
             to_approve.append(str(item))
 
         verify("\n".join(to_approve))
