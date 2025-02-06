72d26ee93f312fc093e6e365876d79b60b532be7
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 15:16:37 +1030

    r rm redundant else

--- gilded_rose.py
+++ gilded_rose.py
@@ -25,27 +25,26 @@ def update_item(item: Item):
                 item.quality = item.quality + 1
         return
 
-    else:
-        if is_backstage_pass:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                item.quality = item.quality - item.quality
-            return
-
-        if item.quality > 0:
-            item.quality = item.quality - 1
+    if is_backstage_pass:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.sell_in < 11:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
-            if item.quality > 0:
-                item.quality = item.quality - 1
+            item.quality = item.quality - item.quality
+        return
+
+    if item.quality > 0:
+        item.quality = item.quality - 1
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0:
+        if item.quality > 0:
+            item.quality = item.quality - 1
 
 
 class GildedRose:
