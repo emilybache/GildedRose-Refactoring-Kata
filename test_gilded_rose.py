00dc716d3bd7e84d2e3d08466c5eeb87c91e6b98
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 16:07:25 +1030

    r rm unittest

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,30 +1,28 @@
-import unittest
-
 from gilded_rose import Item, GildedRose
 from approvaltests.combination_approvals import verify_all_combinations
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_update_quality(self):
-        input_names = [
-            "foo",
-            "Aged Brie",
-            "Backstage passes to a TAFKAL80ETC concert",
-            "Sulfuras, Hand of Ragnaros",
-        ]
-        input_qualities = [-1, 0, 1, 49, 50, 51]
-        input_sell_ins = [-1, 0, 1, 10, 11, 12]
+def test_update_quality():
+    input_names = [
+        "foo",
+        "Aged Brie",
+        "Backstage passes to a TAFKAL80ETC concert",
+        "Sulfuras, Hand of Ragnaros",
+    ]
+    input_qualities = [-1, 0, 1, 49, 50, 51]
+    input_sell_ins = [-1, 0, 1, 10, 11, 12]
 
-        verify_all_combinations(
-            self.do_stuff,
-            [
-                input_names,
-                input_sell_ins,
-                input_qualities,
-            ],
-        )
+    verify_all_combinations(
+        do_stuff,
+        [
+            input_names,
+            input_sell_ins,
+            input_qualities,
+        ],
+    )
 
-    def do_stuff(self, name, sell_in, quality):
-        item = Item(name, sell_in, quality)
-        GildedRose().update_quality([item])
-        return str(item)
+
+def do_stuff(name, sell_in, quality):
+    item = Item(name, sell_in, quality)
+    GildedRose().update_quality([item])
+    return str(item)
