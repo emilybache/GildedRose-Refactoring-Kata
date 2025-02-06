421ce7feb58445d83b30bf22c0a22e498741c78d
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 15:15:19 +1030

    r move sulfuras up

--- gilded_rose.py
+++ gilded_rose.py
@@ -10,6 +10,10 @@ class Item:
 
 def update_item(item: Item):
     is_aged_brie = item.name == "Aged Brie"
+    is_sulfuras = item.name == "Sulfuras, Hand of Ragnaros"
+
+    if is_sulfuras:
+        return
 
     if is_aged_brie:
         if item.quality < 50:
@@ -36,11 +40,6 @@ def update_item(item: Item):
                 item.quality = item.quality - item.quality
 
         else:
-            is_sulfuras = item.name == "Sulfuras, Hand of Ragnaros"
-
-            if is_sulfuras:
-                return
-
             if item.quality > 0:
                 item.quality = item.quality - 1
             item.sell_in = item.sell_in - 1
