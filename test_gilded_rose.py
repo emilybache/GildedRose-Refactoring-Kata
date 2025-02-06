4dc30333526fa9965908f43437bb1a0f04e81c31
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 14:53:14 +1030

    r introduce param

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,8 +5,8 @@ from approvaltests.combination_approvals import verify_all_combinations
 
 
 class GildedRoseTest(unittest.TestCase):
-    def do_stuff(self, name, quality=0):
-        item = Item(name, 0, quality)
+    def do_stuff(self, name, quality=0, sell_in=0):
+        item = Item(name, sell_in, quality)
         gilded_rose = GildedRose([item])
         gilded_rose.update_quality()
         return str(item)
@@ -18,8 +18,9 @@ class GildedRoseTest(unittest.TestCase):
             "Backstage passes to a TAFKAL80ETC concert",
             "Sulfuras, Hand of Ragnaros",
         ]
+        input_qualities = [-1, 0, 1, 49, 50, 51]
 
-        verify_all_combinations(self.do_stuff, [input_names, [-1, 0, 1, 49, 50, 51]])
+        verify_all_combinations(self.do_stuff, [input_names, input_qualities])
 
 
 if __name__ == "__main__":
