9d1928e5e213a4abfd9f6e63502fb7e82ee2bacf
Shesh Santosh <shesh.dishari@protonshub.in>
2025-02-14 16:45:36 +0530

    add multiple conditions of item in texttest_fixture

--- ruby/texttest_fixture.rb
+++ ruby/texttest_fixture.rb
@@ -5,15 +5,23 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 puts "OMGHAI!"
 items = [
   Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
+  Item.new(name="+5 Dexterity Vest", sell_in=0, quality=20),
+  Item.new(name="+5 Dexterity Vest", sell_in=0, quality=0),
   Item.new(name="Aged Brie", sell_in=2, quality=0),
+  Item.new(name="Aged Brie", sell_in=0, quality=9),
+  Item.new(name="Aged Brie", sell_in=2, quality=49),
   Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
   Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
   Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-  # This Conjured item does not work properly yet
-  Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=45),
+  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=40),
+  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=3),
+  Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), 
+  Item.new(name="Conjured Mana Cake", sell_in=0, quality=6),
+  Item.new(name="Conjured Mana Cake", sell_in=5, quality=1),
 ]
 
 days = 2
