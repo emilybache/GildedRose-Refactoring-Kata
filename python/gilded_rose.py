eac39fff23a904f824125da66f8c725416e0bf2a
Teena <teenakhandelwal03@gmail.com>
2025-02-26 16:17:32 +0530

    Refactor, reduce nested conditions and  modularize code

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,59 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
-
+class GildedRose:
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue  # Legendary item, no changes
+
+            self.update_item(item)
+
+    def update_item(self, item):
+        if item.name == "Aged Brie":
+            self.update_aged_brie(item)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            self.update_backstage_passes(item)
+        else:
+            self.update_regular_item(item)
+
+        # Reduce sell_in for all items except "Sulfuras"
+        item.sell_in -= 1
+
+        # Handle expired items
+        if item.sell_in < 0:
+            self.handle_expired_item(item)
+
+    def update_aged_brie(self, item):
+        self.increase_quality(item)
+
+    def update_backstage_passes(self, item):
+        self.increase_quality(item)
+        if item.sell_in < 11:
+            self.increase_quality(item)
+        if item.sell_in < 6:
+            self.increase_quality(item)
+
+    def update_regular_item(self, item):
+        self.decrease_quality(item)
+
+    def handle_expired_item(self, item):
+        if item.name == "Aged Brie":
+            self.increase_quality(item)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            item.quality = 0
+        else:
+            self.decrease_quality(item)
+
+    def increase_quality(self, item):
+        if item.quality < 50:
+            item.quality += 1
+
+    def decrease_quality(self, item):
+        if item.quality > 0:
+            item.quality -= 1
 
 
 class Item:
@@ -43,4 +63,4 @@ class Item:
         self.quality = quality
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+        return f"{self.name}, {self.sell_in}, {self.quality}"
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,65 @@
 # -*- coding: utf-8 -*-
 import unittest
-
 from gilded_rose import Item, GildedRose
 
-
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+    def test_regular_item(self):
+        items = [Item("+5 Dexterity Vest", sell_in=10, quality=20)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].sell_in, 9)
+        self.assertEqual(items[0].quality, 19)
 
-        
+    def test_aged_brie_increases_in_quality(self):
+        items = [Item("Aged Brie", sell_in=2, quality=0)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].sell_in, 1)
+        self.assertEqual(items[0].quality, 1)
+
+    def test_sulfuras_does_not_decrease(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].sell_in, 0)
+        self.assertEqual(items[0].quality, 80)
+
+    def test_backstage_passes_increase_in_quality(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].sell_in, 9)
+        self.assertEqual(items[0].quality, 22)  # Increases by 2 when sell_in <= 10
+
+    def test_backstage_passes_drop_to_zero_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=30)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].sell_in, -1)
+        self.assertEqual(items[0].quality, 0)
+
+    def test_quality_never_exceeds_50(self):
+        items = [Item("Aged Brie", sell_in=5, quality=50)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].quality, 50)
+
+    def test_quality_never_goes_below_zero(self):
+        items = [Item("+5 Dexterity Vest", sell_in=5, quality=0)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].quality, 0)
+
+    def test_expired_item_degrades_twice_as_fast(self):
+        items = [Item("+5 Dexterity Vest", sell_in=0, quality=10)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].sell_in, -1)
+        self.assertEqual(items[0].quality, 8)  # Decreases by 2 after expiry
+
+    def test_conjured_item_degrades_twice_as_fast(self):
+        items = [Item("Conjured Mana Cake", sell_in=3, quality=6)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].sell_in, 2)
+        self.assertEqual(items[0].quality, 5)
+
+    def test_conjured_item_degrades_four_times_as_fast_when_expired(self):
+        items = [Item("Conjured Mana Cake", sell_in=0, quality=6)]
+        GildedRose(items).update_quality()
+        self.assertEqual(items[0].sell_in, -1)
+        self.assertEqual(items[0].quality, 4)
+
 if __name__ == '__main__':
     unittest.main()
