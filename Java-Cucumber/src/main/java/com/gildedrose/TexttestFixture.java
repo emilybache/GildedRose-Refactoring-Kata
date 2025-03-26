68718829775cec9437189e5828e6e6bfb587e911
jns9j04 <joris.nens@colruytgroup.com>
2025-03-26 11:14:08 +0100

    moded to Java folder

--- Java-Cucumber/src/main/java/com/gildedrose/TexttestFixture.java
+++ Java-Cucumber/src/main/java/com/gildedrose/TexttestFixture.java
@@ -18,7 +18,7 @@ public class TexttestFixture {
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
+        int days = 30;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }
---
+++ Java/src/test/java/com/gildedrose/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package com.gildedrose;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-report.html"},
+        features = {"src/test/resources"})
+public class RunCucumberTest {
+}
+
