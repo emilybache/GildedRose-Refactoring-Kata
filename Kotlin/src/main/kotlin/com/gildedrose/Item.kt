430300480840d3aad000c8905bd7961a9a862bfa
Denys <pedan.denys@gmail.com>
2025-05-10 19:44:21 +1200

    Check git works

--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Item.kt
@@ -1,5 +1,6 @@
 package com.gildedrose
 
+//Check git works
 open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
