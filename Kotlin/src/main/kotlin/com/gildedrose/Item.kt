85de84bb8bbe8bab3263289eab4cd325a75b0a80
Denys <pedan.denys@gmail.com>
2025-05-10 23:43:24 +1200

    Data Class

--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Item.kt
@@ -1,9 +1,10 @@
 package com.gildedrose
 
 //Check git works
-open class Item(var name: String, var sellIn: Int, var quality: Int) {
-    override fun toString(): String {
-        return this.name + ", " + this.sellIn + ", " + this.quality
-    }
-}
+//open class Item(var name: String, var sellIn: Int, var quality: Int) {
+//    override fun toString(): String {
+//        return this.name + ", " + this.sellIn + ", " + this.quality
+//    }
+//}
 
+data class Item(var name: String, var sellIn: Int, var quality: Int)
