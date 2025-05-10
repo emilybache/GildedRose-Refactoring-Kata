e3cd8fe4d85320c2adf402b9b524db89d24506ab
Denys <pedan.denys@gmail.com>
2025-05-11 11:47:37 +1200

    When instead of if

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -23,12 +23,12 @@ class GildedRose(var items: List<Item>) {
                             items[i].quality = upgradeQuality(items[i].quality)
                             items[i].quality = upgradeQuality(items[i].quality)
                         }
+
                         items[i].sellIn < 11 -> {
                             items[i].quality = upgradeQuality(items[i].quality)
                         }
                     }
                 }
-
             }
 
             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
@@ -36,18 +36,20 @@ class GildedRose(var items: List<Item>) {
             }
 
             if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                when (items[i].name) {
+                    "Aged Brie" -> {
+                        items[i].quality = upgradeQuality(items[i].quality)
+                    }
+                    "Backstage passes to a TAFKAL80ETC concert" -> {
+                        items[i].quality -= items[i].quality
+                    }
+                    else -> {
                         if (items[i].quality > 0) {
                             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+                                items[i].quality -= 1
                             }
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
                     }
-                } else {
-                    items[i].quality = upgradeQuality(items[i].quality)
                 }
             }
         }
