596ae4de8bca2af480f6b90e242e968948b3e459
iqbal201283 <iqbal201283@gmail.com>
2025-06-22 13:45:08 +0530

    Iqbal Refactoring

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from items import Item
 
 class GildedRose(object):
 
@@ -36,11 +37,11 @@ class GildedRose(object):
                         item.quality = item.quality + 1
 
 
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
+# class Item:
+#     def __init__(self, name, sell_in, quality):
+#         self.name = name
+#         self.sell_in = sell_in
+#         self.quality = quality
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+#     def __repr__(self):
+#         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
