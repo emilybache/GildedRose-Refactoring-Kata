7461de47196a90235cfb6f1074a1c2a7eb6bf99c
iqbal201283 <iqbal201283@gmail.com>
2025-06-22 13:50:10 +0530

    Iqbal Refactoring

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -27,7 +27,22 @@ class AgedBrieUpdater(BaseItemUpdater):
         if self.item.sell_in < 0 and self.item.quality < 50:
             self.item.quality += 1
 
-            
+
+
+class BackstagePassUpdater(BaseItemUpdater):
+    def update_quality(self):
+        if self.item.sell_in < 0:
+            self.item.quality = 0
+        elif self.item.sell_in < 5:
+            self.item.quality = min(self.item.quality + 3, 50)
+        elif self.item.sell_in < 10:
+            self.item.quality = min(self.item.quality + 2, 50)
+        else:
+            if self.item.quality < 50:
+                self.item.quality += 1
+
+
+
 # class GildedRose(object):
 
 #     def __init__(self, items):
