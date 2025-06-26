a640d531326b55082501b8728eb7287c66c79131
Raj <142721365+raj90762130825@users.noreply.github.com>
2025-06-27 01:21:10 +0530

    Update gilded_rose.py

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,6 +7,10 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            if item.name == "Raj" and item.name != "Backstage passes to a TAFKAL80ETC concert": #added a condition for Raj
+                if item.quality > 0:
+                    if item.name == "Raj":
+                        item.quality = item.quality - 1
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
