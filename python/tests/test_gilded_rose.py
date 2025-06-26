fe304741461646ab252008061cdc12806d0aefc1
Raj <142721365+raj90762130825@users.noreply.github.com>
2025-06-27 01:21:49 +0530

    Update test_gilded_rose.py

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -9,7 +9,20 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        #self.assertEqual("fixme", items[0].name) #this line was causing an error so commented this
+        self.assertEqual("foo", items[0].name) #instead added this line
+    
+    def test_conjured_before_sell_date(): #added this function
+        item = Item("Raj", 5, 10)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertEqual(item.quality, 8)
+
+    def test_conjured_after_sell_date():  #added this function
+        item = Item("Raj", 0, 10)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertEqual(item.quality, 6)
 
         
 if __name__ == '__main__':
