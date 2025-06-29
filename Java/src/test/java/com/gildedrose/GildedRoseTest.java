b2a06a1b9defaa24778df1395deceac37285780a
villanibe <thiago-augusto.villani@capgemini.com>
2025-06-30 01:55:59 +0200

    Revertind delete

---
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+import com.gildedrose.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+    }
+
+}
