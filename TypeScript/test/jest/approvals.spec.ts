40c997220d26325b3038d02cdf96a9430c95ced6
Arno Chauveau <arno.chauveau@axxes.com>
2025-07-23 15:15:32 +0200

    refactor snapshot tests
    - refactor snapshot tests to not use the console output and text test. 
    - Remove texttest rig python script
    - generate snapshot file

--- TypeScript/test/jest/approvals.spec.ts
+++ TypeScript/test/jest/approvals.spec.ts
@@ -1,54 +1,27 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-/**
- * This unit test uses [Jest Snapshot](https://goo.gl/fbAQLP).
- * 
- * There are two test cases here with different styles:
- * <li>"foo" is more similar to the unit test from the 'Java' version
- * <li>"thirtyDays" is more similar to the TextTest from the 'Java' version
- *
- * I suggest choosing one style to develop and deleting the other.
- */
+describe("Gilded Rose Approval", () => {
+  it("should match the snapshot for thirty Days", () => {
+    const items = [
+      new Item("+5 Dexterity Vest", 10, 20), //
+      new Item("Aged Brie", 2, 0), //
+      new Item("Elixir of the Mongoose", 5, 7), //
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      // this conjured item does not work properly yet
+      new Item("Conjured Mana Cake", 3, 6),
+    ];
 
-describe('Gilded Rose Approval', () => {
+    const gildedRose = new GildedRose(items);
 
-  let gameConsoleOutput: string;
-  let originalConsoleLog: (message: any) => void;
-  let originalProcessArgv: string[]
+    const days = 30;
 
-  function gameConsoleLog(msg: string) {
-    if (msg) {
-      gameConsoleOutput += msg;
+    for (let i = 0; i < days; i++) {
+      const updatedItems = gildedRose.updateQuality();
+      expect(updatedItems).toMatchSnapshot();
     }
-    gameConsoleOutput += "\n";
-  }
-
-  beforeEach(() => {
-    // prepare capturing console.log to our own gameConsoleLog.
-    gameConsoleOutput = "";
-    originalConsoleLog = console.log;
-    console.log = gameConsoleLog;
-    originalProcessArgv = process.argv;
   });
-
-  afterEach(() => {
-    // reset original console.log
-    console.log = originalConsoleLog;
-    process.argv = originalProcessArgv;
-  });
-
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-  
-    expect(items).toMatchSnapshot();
-  });
-
-  it('should thirtyDays', () => {
-    process.argv = ["<node>", "<script", "30"];
-    require('../golden-master-text-test.ts');
-       
-    expect(gameConsoleOutput).toMatchSnapshot();
-  });
-
 });
