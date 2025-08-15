a506e0313acbcdbc3112fef3e8e7a64d1023438e
kahsez <kahsez@users.noreply.github.com>
2025-08-15 13:25:00 +0200

    test(elixir): approval test for 30 days

--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -1,6 +1,13 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
+  import ExUnit.CaptureIO
+
+  test "Approval test 30 days" do
+    expected = File.read!("test/ApprovalTest.ThirtyDays.verified.txt")
+    assert capture_io(fn -> GildedRose.TextTestFixture.run(30) end) == expected
+  end
+
   test "begin the journey of refactoring" do
     items = [%Item{name: "foo", sell_in: 0, quality: 0}]
     GildedRose.update_quality(items)
