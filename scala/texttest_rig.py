5904431bde9d4dc46c76f931eda869a76e35be0f
Ralf Comtesse <rcc@ralf-comtesse.de>
2025-09-26 21:14:05 +0200

    removed extra, unnecessary run in texttest_rig.py

--- scala/texttest_rig.py
+++ scala/texttest_rig.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-This script uses Gradle to execute the TexttestFixture.
+This script uses sbt to execute the TexttestFixture.
 It is designed to be used by TextTest and specified in the file 'texttests/config.gr' in this repo.
 It is more convenient for TextTest to use since Gradle needs
 several arguments in addition to the one the TextTest fixture needs.
@@ -11,4 +11,4 @@ import sys
 
 args = " ".join(sys.argv[1:])
 TEXTTEST_HOME = os.environ.get("TEXTTEST_HOME", os.getcwd())
-subprocess.run(f"""(cd {TEXTTEST_HOME}/scala/; sbt run "Test / runMain com.gildedrose.TexttestFixture {args}") """, shell=True)
+subprocess.run(f"""(cd {TEXTTEST_HOME}/scala/; sbt "Test / runMain com.gildedrose.TexttestFixture {args}") """, shell=True)
