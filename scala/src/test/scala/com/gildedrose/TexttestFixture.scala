db3b9ef3da9c9185b655cccacacc3f255a97da9c
Ralf Comtesse <rcc@ralf-comtesse.de>
2025-09-28 10:49:02 +0200

    removed unnecessary comment markers at line ends in TexttestFixture.scala

--- scala/src/test/scala/com/gildedrose/TexttestFixture.scala
+++ scala/src/test/scala/com/gildedrose/TexttestFixture.scala
@@ -6,10 +6,10 @@ object TexttestFixture {
     println("OMGHAI!")
 
     val items = Array[Item](
-        Item("+5 Dexterity Vest", 10, 20),         //
-        Item("Aged Brie", 2, 0),                   //
-        Item("Elixir of the Mongoose", 5, 7),      //
-        Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        Item("+5 Dexterity Vest", 10, 20),
+        Item("Aged Brie", 2, 0),
+        Item("Elixir of the Mongoose", 5, 7),
+        Item("Sulfuras, Hand of Ragnaros", 0, 80),
         Item("Sulfuras, Hand of Ragnaros", -1, 80),
         Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
         Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
