639c709c995129f556a189e5e09d4dd468be88f9
Ralf Comtesse <rcc@ralf-comtesse.de>
2025-10-06 08:22:51 +0200

    Housekeeping

--- scala/build.sbt
+++ scala/build.sbt
@@ -7,12 +7,11 @@ lazy val root = project
       version      := "1.0",
       scalaVersion := scala3Version,
       libraryDependencies ++= Seq(
-          "org.scalatest"    %% "scalatest"     % "3.2.19" % Test,
-          "com.approvaltests" % "approvaltests" % "25.4.3" % Test,
-          "junit" % "junit" % "4.13.2",
+          "org.scalatest"       %% "scalatest"         % "3.2.19" % Test,
+          "com.approvaltests"    % "approvaltests"     % "25.4.3" % Test,
+          "junit"                % "junit"             % "4.13.2",
           "com.github.sbt.junit" % "jupiter-interface" % "0.15.1" % Test,
-          "org.junit.jupiter"    % "junit-jupiter"     % "6.0.0"                          % Test
+          "org.junit.jupiter"    % "junit-jupiter"     % "6.0.0"  % Test
       ),
-    testOptions += Tests.Argument(TestFrameworks.JUnit)
-
+      testOptions += Tests.Argument(TestFrameworks.JUnit)
   )
--- scala/src/main/scala/com/gildedrose/Main.scala
+++ scala/src/main/scala/com/gildedrose/Main.scala
@@ -3,16 +3,16 @@ package com.gildedrose
 object Main {
   def main(args: Array[String]): Unit = {
     val items = Array[Item](
-      new Item("+5 Dexterity Vest", 10, 20),
-      new Item("Aged Brie", 2, 0),
-      new Item("Elixir of the Mongoose", 5, 7),
-      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      Item("+5 Dexterity Vest", 10, 20),
+      Item("Aged Brie", 2, 0),
+      Item("Elixir of the Mongoose", 5, 7),
+      Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
       // this conjured item does not work properly yet
-      new Item("Conjured Mana Cake", 3, 6)
+      Item("Conjured Mana Cake", 3, 6)
     )
 
     val app = new GildedRose(items)
