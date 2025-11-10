d700742aa67a2715e8cb81772b8ea22b5af415cf
zoengsw <zoe.ng@softwire.com>
2025-11-10 16:39:25 +0000

    refactor: helper functions in incrementing and decrementing

--- python/tests/gilded_rose.py
+++ python/tests/gilded_rose.py
@@ -4,13 +4,19 @@ class GildedRose(object):
     BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert'
     SULFURAS = 'Sulfuras, Hand of Ragnaros'
     AGED_BRIE = 'Aged Brie'
+    MAX_QUAL = 50
 
     def __init__(self, items):
         self.items = items
 
+    def __qual_inc(self, item, inc):
+        item.quality = max(50, item.quality + inc)
+
+    def __qual_dec(self, item, dec):
+        item.quality = min(0, item.quality - dec)
+
     def update_quality(self):
         for item in self.items:
-            
             match item.name:
                 case self.AGED_BRIE:
                     pass
@@ -19,34 +25,36 @@ class GildedRose(object):
                 case self.BACKSTAGE:
                     pass
                 case _:
-                    pass
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+                    self.__qual_dec(item, 1)
+            if not item.name == self.SULFURAS:
                 item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            # if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            #     if item.quality > 0:
+            #         if item.name != "Sulfuras, Hand of Ragnaros":
+            #             item.quality = item.quality - 1
+            # else:
+            #     if item.quality < 50:
+            #         item.quality = item.quality + 1
+            #         if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            #             if item.sell_in < 11:
+            #                 if item.quality < 50:
+            #                     item.quality = item.quality + 1
+            #             if item.sell_in < 6:
+            #                 if item.quality < 50:
+            #                     item.quality = item.quality + 1
+            # if item.name != "Sulfuras, Hand of Ragnaros":
+            #     item.sell_in = item.sell_in - 1
+            # if item.sell_in < 0:
+            #     if item.name != "Aged Brie":
+            #         if item.name != "Backstage passes to a TAFKAL80ETC concert":
+            #             if item.quality > 0:
+            #                 if item.name != "Sulfuras, Hand of Ragnaros":
+            #                     item.quality = item.quality - 1
+            #         else:
+            #             item.quality = item.quality - item.quality
+            #     else:
+            #         if item.quality < 50:
+            #             item.quality = item.quality + 1
 
 
 class Item:
