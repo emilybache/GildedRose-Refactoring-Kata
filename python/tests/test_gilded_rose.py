9ac033e03827ebd89cf741c9ac3436489ffbf309
zoengsw <zoe.ng@softwire.com>
2025-11-10 14:02:13 +0000

    added 2 tests and 1 helper function on update

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -11,16 +11,35 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(item.sell_in, sell_in)
         self.assertEqual(item.quality, quality)
 
+    def generate_and_update_gilded_rose(self, items, days):
+        gilded_rose = GildedRose(items)
+        for day in range(days):
+            gilded_rose.update_gilded_rose()
+        return gilded_rose
+
+    def test_foo_1day(self):
+        items= [Item('foo', 1, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.check_item_values(items[0], 'foo', 0, 0)
+
+
     def test_foo_nonnegative(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
-        self.assertEqual(0, items[0].sell_in)
-        self.assertEqual(0, items[0].quality)
+        self.check_item_values(items[0], "foo", 0, 0)
+
 
     def test_multiple_items(self):
-        items = [Item("foo", 0, 0), Item("bar", 0, 0)]
+        items = [Item("foo", 0, 0), Item("bar", 1, 1), Item("baz", 2, 2)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.check_item_values(items[0], "foo", 0, 0)
+        self.check_item_values(items[1], "bar", 0, 0)
+        self.check_item_values(items[2], "baz", 1, 1)
+
+
         
 if __name__ == '__main__':
     unittest.main()
