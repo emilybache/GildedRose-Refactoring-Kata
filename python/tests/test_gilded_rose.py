f37e54b5ad45ca1e233c4d08f74f9a39e42ff536
zoengsw <zoe.ng@softwire.com>
2025-11-10 13:57:45 +0000

    added test helper function to check all name, sellin, quality

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -5,12 +5,22 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
+
+    def check_item_values(self, item, name, sell_in, quality):
+        self.assertEqual(item.name, name)
+        self.assertEqual(item.sell_in, sell_in)
+        self.assertEqual(item.quality, quality)
+
+    def test_foo_nonnegative(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
+        self.assertEqual(0, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
 
+    def test_multiple_items(self):
+        items = [Item("foo", 0, 0), Item("bar", 0, 0)]
         
 if __name__ == '__main__':
     unittest.main()
