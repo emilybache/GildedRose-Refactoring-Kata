96fbc5ca78b4c85424cf868b2c2776b2b821dc33
Yossri Ghabri <yossrighabri@gmail.com>
2025-11-25 15:52:32 +0100

    Add unit tests for GildedRose updateQuality for each item type

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,37 @@ class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    void testUpdateQualityNormalItem() {
+        Item[] items = new Item[] { new Item("foo", 5, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(9, items[0].quality);
+        assertEquals(4, items[0].sellIn);
+    }
+
+    @Test
+    void testUpdateQualityAgedBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(11, items[0].quality);
+        assertEquals(4, items[0].sellIn);
+    }
+
+    @Test
+    void testUpdateQualitySulfuras() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(80, items[0].quality);
+        assertEquals(0, items[0].sellIn);
+    }
+
 }
