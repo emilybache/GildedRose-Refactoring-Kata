8526bf2af5b19d193ef061f783819e0f0aefc1de
Karim Fadel <karimmm.fadel@gmail.com>
2026-01-14 00:01:36 +0200

    reformat code

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -14,30 +14,30 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (items[i].name.equals(AGED_BRIE)
                 || items[i].name.equals(BACKSTAGE_PASSES)) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
 
-                        if (items[i].name.equals(BACKSTAGE_PASSES)) {
-                            if (items[i].sellIn < 11) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
                             }
+                        }
 
-                            if (items[i].sellIn < 6) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
                             }
                         }
                     }
-                } else {
-            if (items[i].quality > 0) {
-                if (!items[i].name.equals(SULFURAS)) {
-                    items[i].quality = items[i].quality - 1;
+                }
+            } else {
+                if (items[i].quality > 0) {
+                    if (!items[i].name.equals(SULFURAS)) {
+                        items[i].quality = items[i].quality - 1;
+                    }
                 }
             }
-        }
 
             if (!items[i].name.equals(SULFURAS)) {
                 items[i].sellIn = items[i].sellIn - 1;
