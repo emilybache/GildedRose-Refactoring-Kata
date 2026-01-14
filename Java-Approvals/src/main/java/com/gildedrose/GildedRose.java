3ab5fb11d18e2787c9911ebbfac19915ae292cac
Karim Fadel <karimmm.fadel@gmail.com>
2026-01-14 06:55:07 +0200

    split || if condition

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -12,8 +12,21 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals(AGED_BRIE)
-                || items[i].name.equals(BACKSTAGE_PASSES)) {
+            if (items[i].name.equals(AGED_BRIE)) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
+                        if (items[i].sellIn < 11) {
+                            incrementQualityByOne(i);
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            incrementQualityByOne(i);
+                        }
+                    }
+                }
+            } else if (items[i].name.equals(BACKSTAGE_PASSES)) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
@@ -28,11 +41,7 @@ class GildedRose {
                     }
                 }
             } else {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+                decreaseQualityByOne(i);
             }
 
             if (!items[i].name.equals(SULFURAS)) {
@@ -46,13 +55,17 @@ class GildedRose {
                     if (items[i].name.equals(BACKSTAGE_PASSES)) {
                         items[i].quality = 0;
                     } else {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+                        decreaseQualityByOne(i);
                     }
                 }
+            }
+        }
+    }
+
+    private void decreaseQualityByOne(int i) {
+        if (items[i].quality > 0) {
+            if (!items[i].name.equals(SULFURAS)) {
+                items[i].quality = items[i].quality - 1;
             }
         }
     }
