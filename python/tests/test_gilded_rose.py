6c8fe48612a3ac48a2e20f6955b08ad136e55d9a
Raj Vora <rvvora@uci.edu>
2026-05-05 13:28:00 -0700

    fix: correct placeholder test assertion and exclude uninstalled approvals test

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
+import sys
 import unittest
+from pathlib import Path
+
+_EXERCISE_ROOT = Path(__file__).resolve().parent.parent
+if str(_EXERCISE_ROOT) not in sys.path:
+    sys.path.insert(0, str(_EXERCISE_ROOT))
 
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
+    def test_normal_item_at_zero_quality_stays_zero(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual(0, items[0].quality)
 
         
 if __name__ == '__main__':
